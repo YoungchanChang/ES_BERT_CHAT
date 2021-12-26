4523ef6b2f2b62de3016f610c81003e561b26676
youngchan <youngchanchatbot@gmail.com>
2021-12-26 12:21:57 +0900

    Add test_extract_value.py to get test file
    - 테스트 기준이 되는 문장을 위한 코드
    - json파일에서 원하는 문장, 엔티티만 추출

---
+++ mecab_value_extractor_dir/test_extract_value.py
@@ -0,0 +1,31 @@
+"""
+call_center data source : https://aihub.or.kr/aidata/30716
+"""
+import os
+import json
+import csv
+
+def get_sentence_entities():
+    for (path, dir, files) in os.walk("./"):
+        for filename in files:
+            ext = os.path.splitext(filename)[-1]
+            if ext == '.json':
+                print("%s/%s" % (path, filename))
+                with open(path + "/" + filename, 'r', encoding='cp949') as f:
+                    json_data_list = json.load(f)
+                    question_list = [json_data_item for json_data_item in json_data_list if json_data_item.get('QA') == "Q"]
+                    question_values = [((question_item.get('고객질문(요청)').strip() or question_item.get('상담사질문(요청)').strip()), question_item.get('개체명 ')) for question_item in question_list if question_item.get('개체명 ') != '']
+
+    return question_values
+
+
+def write_csv(csv_list):
+    with open('./test_file/call_center.csv', 'w', encoding='utf-8-sig', newline='') as writer_csv:
+        writer = csv.writer(writer_csv, delimiter=',')
+        for csv_item in csv_list:
+            writer.writerow(csv_item)
+
+
+if __name__ == "__main__":
+    sentence_entites_list = get_sentence_entities()
+    write_csv(sentence_entites_list)
