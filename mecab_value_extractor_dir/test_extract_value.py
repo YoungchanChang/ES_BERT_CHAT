5385ad7d8c0e8f270b4d9808faf91ba98c1cf08b
youngchan <san0558@naver.com>
2022-01-17 16:12:58 +0900

    Add search_tsv in test_extract_value.py
    - 디렉토리에서 값을 읽은 뒤 mecab으로 분석된 값을 저장하는 함수 추가

--- mecab_value_extractor_dir/test_extract_value.py
+++ mecab_value_extractor_dir/test_extract_value.py
@@ -5,12 +5,17 @@ import os
 import json
 import csv
 import re
+import pandas as pd
+import numpy as np
 import mecab_value_extractor as mve
 USER_SENTENCE = 1
 ENTITY = 2
 QA_CUSTOMER = 0
 QA_SERVICE = 1
 FIRST_VAL = 0
+DIR_NAME = 0
+CATEGORY = 3
+MECAB_PARSE = 4
 BLANK_LIST = []
 
 
@@ -86,22 +91,47 @@ def write_csv(csv_list):
 
 
 def read_csv():
-    with open('./test_file/call_center.csv', 'r', encoding='utf-8-sig') as reader_csv:
+    with open('./test_file/test.csv', 'r', encoding='utf-8-sig') as reader_csv:
         reader = csv.reader(reader_csv, delimiter=',')
         return list(reader)
 
 
 def read_txt():
-    with open("./entity_dir/call_center_entity_mecab.txt", "r", encoding='utf-8-sig') as file:
+    with open("entity_dir/entity_dump/call_center_entity_mecab.txt", "r", encoding='utf-8-sig') as file:
         txt_list = file.read().splitlines()
         return sorted(list(txt_list), key=len, reverse=True)
 
 
-def write_txt(txt_list):
-    with open("./entity_dir/call_center_entity_mecab.txt", "w", encoding='UTF8') as file:
+def write_txt(dir_name, txt_list):
+    with open(f"entity_dir/entity_mecab/{dir_name}_mecab.txt", "w", encoding='UTF8') as file:
         for txt_item in txt_list:
             data = txt_item + "\n"
             file.write(data)
+
+
+def search_tsv():
+    dirname = "./entity_dir/entity_original"
+    filenames = os.listdir(dirname)
+    for filename in filenames:
+        full_filename = os.path.join(dirname, filename)
+        ext = os.path.splitext(full_filename)[-1]
+
+        category_list = []
+        # 1. tsv파일 읽기
+        if ext == '.tsv':
+            # 1-1. 이름에 따라 대분류, 소분류 구분
+            file_name = os.path.splitext(filename)[0]
+            entity, large_category, sub_category = file_name.split("_")
+            df = pd.read_csv(full_filename, sep='\t', encoding='utf-8-sig')
+
+            # 1-2. 모든 컬럼에 대해 수행
+            for df_column_item in df.columns:
+                for df_row in df[df_column_item]:
+                    if df_row is np.nan:
+                        break
+                    category_list.append([file_name, large_category, sub_category, df_column_item, df_row])
+
+        yield category_list
 
 
 def mecab_function_test():
@@ -168,18 +198,21 @@ def str_entity_mecab():
     mecab_value_extractor = mve.MeCabValueExtractor()
     txt_list = []
 
-    for read_item in read_txt():
-        txt_list.append(read_item + "," + " ".join([x[mve.IDX_TOKEN] for x in mecab_value_extractor.parse_compound(read_item)]))
-
-    write_txt(txt_list)
+    for search_list in search_tsv():
+        for search_list_item in search_list:
+            mecab_parsed_value = " ".join([x[mve.IDX_TOKEN] for x in mecab_value_extractor.parse_compound(search_list_item[MECAB_PARSE])])
+            txt_list.append(search_list_item[CATEGORY] + "," + search_list_item[MECAB_PARSE] + "," + mecab_parsed_value)
+        write_txt(search_list[FIRST_VAL][DIR_NAME], txt_list)
 
 
 if __name__ == "__main__":
-    import time
-    st = time.time()
-    mecab_function_diff_test()
-    et = time.time()
-    print(et-st)
+    str_entity_mecab()
+
+    # import time
+    # st = time.time()
+    # mecab_function_diff_test()
+    # et = time.time()
+    # print(et-st)
 
     # # 1. get entity
     # sentence_entity = get_sentence_entities()
