3c292aacf7cb2f712ccddf150ace221fde5b1063
youngchan <san0558@naver.com>
2022-01-19 21:47:30 +0900

    Move test_extract_value.py to mecab_data_generator.py
    - mecab 분석용 데이터 읽어서 분석하는 코드 이전

--- mecab_value_extractor_dir/test_extract_value.py
+++ mecab_value_extractor_dir/test_extract_value.py
@@ -4,9 +4,6 @@ call_center data source : https://aihub.or.kr/aidata/30716
 import os
 import json
 import csv
-import re
-import pandas as pd
-import numpy as np
 import mecab_value_extractor as mve
 USER_SENTENCE = 1
 ENTITY = 2
@@ -109,31 +106,6 @@ def write_txt(dir_name, txt_list):
             file.write(data)
 
 
-def search_tsv():
-    dirname = "./entity_dir/entity_original"
-    filenames = os.listdir(dirname)
-    for filename in filenames:
-        full_filename = os.path.join(dirname, filename)
-        ext = os.path.splitext(full_filename)[-1]
-
-        category_list = []
-        # 1. tsv파일 읽기
-        if ext == '.tsv':
-            # 1-1. 이름에 따라 대분류, 소분류 구분
-            file_name = os.path.splitext(filename)[0]
-            entity, large_category, sub_category = file_name.split("_")
-            df = pd.read_csv(full_filename, sep='\t', encoding='utf-8-sig')
-
-            # 1-2. 모든 컬럼에 대해 수행
-            for df_column_item in df.columns:
-                for df_row in df[df_column_item]:
-                    if df_row is np.nan:
-                        break
-                    category_list.append([file_name, large_category, sub_category, df_column_item, df_row])
-
-        yield category_list
-
-
 def read_mecab_val():
     dirname = "./entity_dir/entity_mecab"
     filenames = os.listdir(dirname)
@@ -225,17 +197,6 @@ def mecab_function_diff_test():
         tmp_list.append([csv_item[USER_SENTENCE], csv_item[ENTITY], ", ".join(entity_contain_list), is_same])
     print(is_same_cnt, "/", len(read_csv()))
     write_csv(tmp_list)
-
-
-def str_entity_mecab():
-    mecab_value_extractor = mve.MeCabValueExtractor()
-    txt_list = []
-
-    for search_list in search_tsv():
-        for search_list_item in search_list:
-            mecab_parsed_value = " ".join([x[mve.IDX_TOKEN] for x in mecab_value_extractor.parse_compound(search_list_item[MECAB_PARSE])])
-            txt_list.append(search_list_item[CATEGORY] + "," + search_list_item[MECAB_PARSE] + "," + mecab_parsed_value)
-        write_txt(search_list[FIRST_VAL][DIR_NAME], txt_list)
 
 
 def read_entity_example():
