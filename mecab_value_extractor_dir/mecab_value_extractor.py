c5a1ef59ad449bf12a81c8e6fbc0bf1d04e3fcc3
youngchan <san0558@naver.com>
2022-01-20 13:54:35 +0900

    Move string_replacer to ner_string.py
    - 문자열 치환 함수 문자열 처리 모듈로 이동

--- mecab_value_extractor_dir/mecab_value_extractor.py
+++ mecab_value_extractor_dir/mecab_value_extractor.py
@@ -77,21 +77,6 @@ class MeCabRestoration:
         return reverse_sentence
 
 
-def string_replacer(original_string, newstring, index, nofail=False):
-    # raise an error if index is outside of the string
-    if not nofail and index not in range(len(original_string)):
-        raise ValueError("index outside given string")
-
-    # if not erroring, but the index is still not in the correct range..
-    if index < 0:  # add it to the beginning
-        return newstring + original_string
-    if index > len(original_string):  # add it to the end
-        return original_string + newstring
-
-    len_new_string = len(newstring)
-    blank_string = len(newstring) * "*"
-    # insert the new string between "slices" of the original
-    return original_string[:index] + blank_string + original_string[index + len_new_string:]
 
 
 def reverse_parse(parse_token):
