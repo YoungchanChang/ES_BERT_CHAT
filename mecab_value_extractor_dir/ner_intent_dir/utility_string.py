1e23de286e739ea6e51496e906e98deeccc25046
youngchan <san0558@naver.com>
2022-01-20 15:24:36 +0900

    Add utility_string.py
    - 자소로 나누어 주는 기능, 받침 확인 기능 있는 문자열 유틸리티 모듈 생성

---
+++ mecab_value_extractor_dir/ner_intent_dir/utility_string.py
@@ -0,0 +1,38 @@
+NO_JONGSUNG = 'ᴕ'
+
+CHOSUNGS = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+JOONGSUNGS = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
+JONGSUNGS = [NO_JONGSUNG, 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ',
+             'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+
+N_CHOSUNGS = 19
+N_JOONGSUNGS = 21
+N_JONGSUNGS = 28
+
+FIRST_HANGUL = 0xAC00  # '가'
+LAST_HANGUL = 0xD7A3  # '힣'
+
+
+def to_jaso(s):
+    result = []
+    for c in s:
+        if ord(c) < FIRST_HANGUL or ord(c) > LAST_HANGUL:  # if a character is a hangul
+            result.append(c)
+        else:
+            code = ord(c) - FIRST_HANGUL
+            jongsung_index = code % N_JONGSUNGS
+            code //= N_JONGSUNGS
+            joongsung_index = code % N_JOONGSUNGS
+            code //= N_JOONGSUNGS
+            chosung_index = code
+
+            result.append(CHOSUNGS[chosung_index])
+            result.append(JOONGSUNGS[joongsung_index])
+            result.append(JONGSUNGS[jongsung_index])
+
+    return ''.join(result)
+
+def has_coda(word):
+    # True : 받침이 없는 경우
+    # False : 받침이 있는 경우
+    return (ord(word[-1]) - 44032) % 28 == 0
