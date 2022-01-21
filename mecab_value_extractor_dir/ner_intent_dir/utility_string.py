0fbad5ee1ca302535887afc75a04ead95ad6065e
youngchan <san0558@naver.com>
2022-01-21 22:50:24 +0900

    Add get_marker in utility_string.py
    - "이"나 "가"를 붙이는 마커 생성

--- mecab_value_extractor_dir/ner_intent_dir/utility_string.py
+++ mecab_value_extractor_dir/ner_intent_dir/utility_string.py
@@ -49,7 +49,18 @@ def to_jaso(s):
 
     return ''.join(result)
 
+
 def has_coda(word):
     # True : 받침이 없는 경우
     # False : 받침이 있는 경우
     return (ord(word[-1]) - 44032) % 28 == 0
+
+
+def get_marker(word, marker):
+    josa_sbj = "이"
+    if marker == "josa_sbj":
+        if has_coda(word):
+            josa_sbj = "가"
+            return josa_sbj
+        return josa_sbj
+    return False
