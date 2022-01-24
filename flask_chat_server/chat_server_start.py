b8c36d239ccb1a58b396d9c6d01c42755801d13c
youngchan <san0558@naver.com>
2022-01-24 23:38:26 +0900

    Fix chat_server_start.py
    - 도커 운영 시에 ValueError: Too many packets in payload 해결
    - 참조 링크 : https://github.com/miguelgrinberg/python-engineio/issues/142

--- flask_chat_server/chat_server_start.py
+++ flask_chat_server/chat_server_start.py
@@ -2,9 +2,13 @@ from flask import Flask
 from flask import render_template
 from flask_socketio import SocketIO
 from utility_dir.docker_net import get_mecab_ner_answer
+from engineio.payload import Payload
+import logging
+
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+Payload.max_decode_packets = 50
 
 @app.route("/")
 def index():
@@ -13,9 +17,11 @@ def index():
 
 @socketio.on("chat")
 def event_handler(json):
-    json["nickname"] = json["nickname"].encode("latin1").decode("utf-8")
-    json_message = json["message"].encode("latin1").decode("utf-8")
+    json["nickname"] = json["nickname"].encode("utf-8").decode("utf-8")
+    json_message = json["message"].encode("utf-8").decode("utf-8")
+    logging.debug(json_message)
     mecab_bert_answer = get_mecab_ner_answer(json_message)
+    logging.debug(mecab_bert_answer)
     socketio.emit("response", {"nickname": "friendbot", "message": mecab_bert_answer})
 
 
@@ -23,5 +29,6 @@ def event_handler(json):
 def connect_handler():
     socketio.emit("response", {"nickname": "", "message": "새로운 유저 입장"})
 
+
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=14000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=9096, debug=True)
