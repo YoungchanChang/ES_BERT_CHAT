5569bd08b34659f0df5c444aa6af895092c93cea
youngchan <san0558@naver.com>
2022-02-09 06:58:06 +0900

    Add MecabWordFeature in entity.py
    - 메캅 단어 속성 분해 결과 엔티티 추가

---
+++ mecab_ner/app/domain/entity.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class MecabDomain:
+    ...
+
+
+@dataclass
+class MecabWordFeature(MecabDomain):
+    word: str
+    pos: str
+    semantic: str
+    has_jongseong: bool
+    reading: str
+    type : str
+    start_pos: str
+    end_pos: str
+    expression: str
+    idx_original: Optional[int] = None
+    idx_pos: Optional[int] = None
+
