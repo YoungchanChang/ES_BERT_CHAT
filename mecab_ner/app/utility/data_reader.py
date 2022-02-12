246fec0b3045a42999e9e5cf34b4cd393fd8448c
youngchan <san0558@naver.com>
2022-02-12 14:37:41 +0900

    Add write_txt in data_reader.py
    - 텍스트에 리스트 쓰는 메소드 추가

--- mecab_ner/app/utility/data_reader.py
+++ mecab_ner/app/utility/data_reader.py
@@ -1,10 +1,6 @@
-from typing import List
 
 
 class DataReader:
-
-    CLASS_BOUNDARY = "#"
-    ITEM_BOUNDARY = ","
 
     @staticmethod
     def read_txt(data_path):
@@ -13,17 +9,11 @@ class DataReader:
             return txt_list
 
     @staticmethod
-    def read_category(data_list: List):
-        header, *other = data_list
-        header = header.replace("#", "").strip()
-        category_list = []
+    def write_txt(data_path, txt_list, is_sort=False):
+        if is_sort:
+            txt_list = sorted(list(txt_list), key=len, reverse=True)
 
-        for data_item in other:
-            if DataReader.CLASS_BOUNDARY in data_item:
-                yield header, sorted(category_list, key=len, reverse=True)
-                header = data_item.replace("#", "").strip()
-                category_list = []
-
-            category_list.append(data_item)
-
-        yield header, sorted(category_list, key=len, reverse=True)
+        with open(data_path, "a", encoding='UTF8') as file:
+            for txt_item in txt_list:
+                data = str(txt_item) + "\n"
+                file.write(data)
