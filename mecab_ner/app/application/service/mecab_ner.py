83cc5e9361cdf2577f2324dd9151107464785fb7
youngchan <san0558@naver.com>
2022-02-13 16:51:37 +0900

    Add gen_integrated_entities in mecab_ner.py
    - 엔티티 통합해서 반환하는 코드 추가

--- mecab_ner/app/application/service/mecab_ner.py
+++ mecab_ner/app/application/service/mecab_ner.py
@@ -2,6 +2,7 @@ from typing import List, Generator
 
 from mecab_ner.app.application.service.mecab_generator import MecabGenerator
 from mecab_ner.app.application.service.mecab_parser import MeCabParser
+from mecab_ner.app.application.service.mecab_storage import MeCabStorage
 from mecab_ner.app.domain.entity import MecabCategory
 from mecab_ner.app.utility.mecab_helper import contain_pattern_list
 import copy
@@ -31,6 +32,35 @@ def infer_entity(mecab_parsed_list: List, mecab_category_item: MecabCategory) ->
         break
     return mecab_category_item
 
+def integrate_many_entity_index(mecab_entity_category_list: List, length: int) -> List:
+    """인덱스 값에 엔티티가 있는 경우, 없는 경우 구분"""
+    blank = [MeCabNer.EMPTY_WORD] * length
+    for mecab_entity_category_item in mecab_entity_category_list:
+        for i in range(mecab_entity_category_item.start_idx, mecab_entity_category_item.end_idx, 1):
+            blank[i] = MeCabNer.FULL_WORD
+    return blank
+
+def gen_integrated_entity(blank_list: List) -> Generator:
+    start_idx = None
+    end_idx = None
+    switch_on = True
+    for idx, item in enumerate(blank_list):
+        if item == MeCabNer.FULL_WORD:
+            end_idx = idx
+
+            if switch_on:
+                start_idx = idx
+
+            switch_on = False
+
+            if idx != len(blank_list) - 1:
+                continue
+
+        if (switch_on is False) and end_idx:
+            yield start_idx, end_idx
+            start_idx = None
+            end_idx = None
+            switch_on = True
 
 class MeCabNer:
     """
@@ -48,6 +78,8 @@ class MeCabNer:
     ENTITY = 0
     INFER_FORWARD = 1
     INFER_BACKWARD = 2
+    EMPTY_WORD = 0
+    FULL_WORD = 1
 
     def __init__(self, storage_mecab_path: str):
 
@@ -115,3 +147,22 @@ class MeCabNer:
 
         for step_idx in range(start_idx, end_idx, self.ONE_WORD):
             yield mecab_parsed_list[step_idx][self.WORD_IDX]
+
+    def gen_integrated_entities(self, sentence, status):
+        mecab_parsed_list = list(MeCabParser(sentence=sentence).gen_mecab_compound_token_feature())
+        mecab_entity_category_list = list(self.get_entities(sentence, status=status))
+
+        many_entity_index_list = integrate_many_entity_index(mecab_entity_category_list, length=len(mecab_parsed_list))
+
+        for integrated_entity_item in gen_integrated_entity(many_entity_index_list):
+            end_idx = integrated_entity_item[1] + 1
+            start_idx = integrated_entity_item[0]
+            mecab_parsed_token = mecab_parsed_list[start_idx:end_idx]
+            restore_tokens = MeCabStorage().reverse_compound_tokens(mecab_parsed_token)
+            restore_sentence = " ".join(restore_tokens)
+            for entity_category_item in mecab_entity_category_list:
+                if entity_category_item.end_idx == end_idx:
+                    yield MecabCategory(large_category=entity_category_item.large_category,
+                                        medium_category=entity_category_item.medium_category,
+                                        small_category=entity_category_item.small_category,
+                                        entity=restore_sentence)
