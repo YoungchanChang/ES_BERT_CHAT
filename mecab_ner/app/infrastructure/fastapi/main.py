d79bd6593924eb8b77aed3d4ba786c84858793c0
youngchan <san0558@naver.com>
2022-02-13 21:28:15 +0900

    Add bert_chat in main.py
    - bert chat 응답 endpoint 추가

--- mecab_ner/app/infrastructure/fastapi/main.py
+++ mecab_ner/app/infrastructure/fastapi/main.py
@@ -2,6 +2,9 @@ import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 from dotenv import load_dotenv
+
+from app.utility.docker_net import get_entity_intent_answer
+
 load_dotenv()
 from app.controller.service.mecab_controller import get_data
 
@@ -11,10 +14,20 @@ class Query(BaseModel):
 app = FastAPI()
 
 from fastapi.encoders import jsonable_encoder
+
+
 @app.post("/mecab_entity")
 async def create_item(sentence: Query):
-    return jsonable_encoder(get_data(sentence=sentence.sentence))
+    return_json = {"answer": jsonable_encoder(get_data(sentence=sentence.sentence))}
+    return jsonable_encoder(return_json)
 
+
+@app.post("/mecab_entity/bert_chat")
+async def bert_chat(sentence: Query):
+    return_json = {"answer": jsonable_encoder(get_data(sentence=sentence.sentence))}
+    return_val = get_entity_intent_answer(return_json)
+    return_json = {"answer": return_val}
+    return jsonable_encoder(return_json)
 
 if __name__ == "__main__":
     uvicorn.run(
