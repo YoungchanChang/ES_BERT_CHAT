4e9d9d5a7360571b4be0f35ccd7987970e3d37dc
youngchan <san0558@naver.com>
2022-02-13 21:27:01 +0900

    Fix get_data in mecab_controller.py
    - None이 나오는 경우 예외 처리

--- mecab_ner/app/controller/service/mecab_controller.py
+++ mecab_ner/app/controller/service/mecab_controller.py
@@ -3,7 +3,7 @@ from dataclasses import asdict
 import numpy as np
 from app.application.service.mecab_ner import MeCabNer
 from app.application.service.mecab_storage import MeCabStorage
-from app.domain.entity import MecabCategory, MeCabEntityIntent
+from app.domain.entity import MecabCategory, MeCabEntityIntent, MeCabEntity, MeCabIntent
 
 
 class MeCabController:
@@ -26,19 +26,16 @@ class MeCabController:
 
         if len(entity_list) == 0:
             for intent_item in intent_list:
-                yield intent_item
+                yield MeCabIntent(intent=intent_item)
         elif len(intent_list) == 0:
             for entity_item in entity_list:
-                yield entity_item
-
+                yield MeCabEntity(entity=entity_item)
         else:
             for intent_item in intent_list:
                 tmp_dis = np.inf
                 mc_en_int = None
                 for entity_item in entity_list:
                     if entity_item.large_category == intent_item.large_category:
-                        print(entity_item)
-                        print(intent_item)
                         e_i_dis = abs(intent_item.start_idx - entity_item.start_idx)
                         if e_i_dis <= tmp_dis:
                             mc_en_int = MeCabEntityIntent(entity_item, intent_item)
@@ -48,6 +45,7 @@ class MeCabController:
 def get_data(sentence):
     return_val = []
     for mecab_item in MeCabController().gen_entity_intent(sentence=sentence):
-        return_val.append(asdict(mecab_item))
+        if mecab_item:
+            return_val.append(asdict(mecab_item))
 
     return return_val
