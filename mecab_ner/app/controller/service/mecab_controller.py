02a6c4b7c526b7fa1dd0a9b549fcd03a026725c6
youngchan <san0558@naver.com>
2022-02-13 17:41:17 +0900

    Fix gen_entity_intent in mecab_controller.py
    - 인텐트와 엔티티 매칭을 위한 메소드 추가

--- mecab_ner/app/controller/service/mecab_controller.py
+++ mecab_ner/app/controller/service/mecab_controller.py
@@ -1,10 +1,9 @@
 from typing import List, Generator
+import numpy as np
 
 from mecab_ner.app.application.service.mecab_ner import MeCabNer
 from mecab_ner.app.application.service.mecab_storage import MeCabStorage
-from mecab_ner.app.domain.entity import MecabCategory
-
-
+from mecab_ner.app.domain.entity import MecabCategory, MeCabEntityIntent
 
 
 def integrate_many_entity_index(mecab_entity_category_list: List, mecab_list_length: int) -> List:
@@ -20,56 +19,33 @@ class MeCabController:
     EMPTY_WORD = 0
     FULL_WORD = 1
 
-    def __init__(self, sentence):
+    def __init__(self):
         entity_storage_path = "/Users/youngchan/Desktop/ES_BERT_CHAT/mecab_ner/datas/entities/mecab_storage"
-        self.mecab_ner = MeCabNer(storage_mecab_path=entity_storage_path, sentence=sentence)
+        self.mecab_entity_ner = MeCabNer(storage_mecab_path=entity_storage_path)
+        entity_storage_path = "/Users/youngchan/Desktop/ES_BERT_CHAT/mecab_ner/datas/intents/mecab_storage"
+        self.mecab_intent_ner = MeCabNer(storage_mecab_path=entity_storage_path)
 
-    def integrate_many_entity_index(self, mecab_entity_category_list: List) -> List:
-        """인덱스 값에 엔티티가 있는 경우, 없는 경우 구분"""
-        blank = [self.EMPTY_WORD] * len(self.mecab_ner.mecab_parsed_list)
-        for mecab_entity_category_item in mecab_entity_category_list:
-            for i in range(mecab_entity_category_item.start_idx, mecab_entity_category_item.end_idx, 1):
-                blank[i] = self.FULL_WORD
-        return blank
+    def gen_entity_intent(self, sentence):
 
-    def gen_entity(self):
+        entity_list = self.mecab_entity_ner.gen_integrated_entities(sentence, status=MeCabNer.INFER_FORWARD)
+        entity_list = list(entity_list)
 
-        mecab_entity_category_list = [self.mecab_ner.infer_entity(x) for x in self.mecab_ner.get_category_entity()]
+        intent_list = self.mecab_intent_ner.gen_integrated_entities(sentence, status=MeCabNer.ENTITY)
+        intent_list = list(intent_list)
 
-        many_entity_index_list = self.integrate_many_entity_index(mecab_entity_category_list)
-
-        for integrated_entity_item in gen_integrated_entity(many_entity_index_list):
-
-            end_idx = integrated_entity_item[1] + 1
-            start_idx = integrated_entity_item[0]
-            mecab_parsed_list = self.mecab_ner.mecab_parsed_list[start_idx:end_idx]
-            restore_tokens = MeCabStorage().reverse_compound_tokens(mecab_parsed_list)
-            restore_sentence = " ".join(restore_tokens)
-            for entity_category_item in mecab_entity_category_list:
-                if entity_category_item.end_idx == end_idx:
-                    yield MecabCategory(large_category=entity_category_item.large_category,
-                                        medium_category=entity_category_item.medium_category,
-                                        small_category=entity_category_item.small_category,
-                                        entity=restore_sentence)
-
-    def gen_integrated_entity(self, blank_list: List) -> Generator:
-        start_idx = None
-        end_idx = None
-        switch_on = True
-        for idx, item in enumerate(blank_list):
-            if item == MeCabController.FULL_WORD:
-                end_idx = idx
-
-                if switch_on:
-                    start_idx = idx
-
-                switch_on = False
-
-                if idx != len(blank_list)-1:
-                    continue
-
-            if (switch_on is False) and end_idx:
-                yield start_idx, end_idx
-                start_idx = None
-                end_idx = None
-                switch_on = True
+        if len(intent_list) == 0:
+            yield entity_list
+        elif len(entity_list) == 0:
+            yield intent_list
+        else:
+            for intent_item in intent_list:
+                tmp_dis = np.inf
+                mc_en_int = None
+                for entity_item in entity_list:
+                    if entity_item.large_category == intent_item.large_category:
+                        print(entity_item)
+                        print(intent_item)
+                        e_i_dis = abs(intent_item.start_idx - entity_item.start_idx)
+                        if e_i_dis <= tmp_dis:
+                            mc_en_int = MeCabEntityIntent(entity_item, intent_item)
+                yield mc_en_int
