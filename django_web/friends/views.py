440cddec447c6abf9bd1c9bc61e8ce82b83aa7d4
youngchan <san0558@naver.com>
2022-02-17 22:55:55 +0900

    Add HomeView
    - 메인 뷰를 위한 뷰 객체 생성

--- django_web/friends/views.py
+++ django_web/friends/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+
+from friends import models
+
+
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = models.Friend
+    paginate_by = 3
+    ordering = "created"
+    context_object_name = "friends"
+    template_name_suffix = '_home'
+
+    def first_photo(self):
+        print(self)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
