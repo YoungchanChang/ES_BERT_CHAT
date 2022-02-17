f713f65db1a9d7eed5a88fd355d0fb804ccf5379
youngchan <san0558@naver.com>
2022-02-17 22:54:09 +0900

    Update Friend in core/models.py
    - 이름, 내용의 프렌드 객체 추가

--- django_web/friends/models.py
+++ django_web/friends/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from core import models as core_models
 # Create your models here.
+
+
+class Friend(core_models.TimeStampedModel):
+
+    """ Item Model Definition """
+
+    name = models.CharField(max_length=100)
+    talk = models.TextField(blank=True, default="")
+
