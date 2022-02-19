39751a98318f78722be55e6685bb0bf7c4597823
youngchan <san0558@naver.com>
2022-02-20 06:14:43 +0900

    Add MecabDataController in mecab_controller.py
    - 메캅 데이터 컨트롤러 추가

--- mecab_ner/app/controller/service/mecab_controller.py
+++ mecab_ner/app/controller/service/mecab_controller.py
@@ -1,9 +1,14 @@
 import os
 from dataclasses import asdict
+from pathlib import Path
+
 import numpy as np
+
+from app.application.service.mecab_generator import MecabGenerator
 from app.application.service.mecab_ner import MeCabNer
 from app.application.service.mecab_storage import MeCabStorage
-from app.domain.entity import MecabCategory, MeCabEntityIntent, MeCabEntity, MeCabIntent
+from app.domain.entity import MecabCategory, MeCabEntityIntent, MeCabEntity, MeCabIntent, CategoryItem
+from app.utility.data_reader import DataReader
 
 
 class MeCabController:
@@ -49,3 +54,33 @@ def get_data(sentence):
             return_val.append(asdict(mecab_item))
 
     return return_val
+
+class MecabDataController:
+    BASE_DIR_PATH = Path(__file__).resolve().parent.parent.parent.parent.joinpath("datas")
+
+    def create_index(self, category_item: CategoryItem):
+
+        category_name = category_item.large_category + "_" + category_item.medium_category + ".txt"
+        small_category_name = "#"+category_item.small_category
+
+        BASE_DIR_PATH = ""
+        if category_item.type == "entity":
+            BASE_DIR_PATH = self.BASE_DIR_PATH.joinpath("entities", "storage").joinpath(category_name)
+
+        elif category_item.type == "intent":
+            BASE_DIR_PATH = self.BASE_DIR_PATH.joinpath("intents")
+
+        if not BASE_DIR_PATH.exists():
+            DataReader.write_txt(str(BASE_DIR_PATH), [small_category_name])
+
+        if BASE_DIR_PATH.exists():
+            return True
+        return False
+    # storage_path = "/Users/youngchan/Desktop/ES_BERT_CHAT/mecab_ner/datas/intents/mecab_storage"
+    # m_g = MecabGenerator(storage_path=storage_path)
+    # for path_item in Path(m_g.mecab_path).iterdir():
+    #     Path(path_item).unlink()
+    # Path(m_g.mecab_path).rmdir()
+    #
+    # # init data
+    # m_g = MecabGenerator(storage_path=storage_path)
