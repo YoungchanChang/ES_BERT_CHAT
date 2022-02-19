2d0ae97fdfb10961f1a85c99c44b621bf5868b62
youngchan <san0558@naver.com>
2022-02-19 11:24:23 +0900

    Update asgi.py
    - websocket asgi 추가

--- django_web/config/asgi.py
+++ django_web/config/asgi.py
@@ -7,8 +7,13 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 django.setup()
+import chats.routing
 
 application = ProtocolTypeRouter({
   "http": AsgiHandler(),
-
+  "websocket": AuthMiddlewareStack(
+    URLRouter(
+      chats.routing.websocket_urlpatterns
+    )
+  ),
 })
