e6f008788f496de056a1ca7e40835f17c87dca49
youngchan <san0558@naver.com>
2022-02-20 06:12:24 +0900

    Add ItemAdmin in mecab_ner
    - 메캅을 컨트롤하기 위한 아이템 관리자 추가

--- django_web/mecab_ner/admin.py
+++ django_web/mecab_ner/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+@admin.register(models.EntityCategoryItem, models.IntentCategoryItem)
+class ItemAdmin(admin.ModelAdmin):
+
+    """ Item Admin Definition """
+
+    list_display = ("small_category", "medium_category", "large_category", "used_by")
+
+    list_filter = (
+        "small_category",
+        "medium_category",
+        "large_category",
+    )
+
+    def used_by(self, obj):
+        return obj.entity.count()
+
+
+
+@admin.register(models.MecabEntity)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = (
+        "word",
+        "category",
+    )
