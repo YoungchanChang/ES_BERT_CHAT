9f864534aedaab8bc9854b3706cf2247791915f9
youngchan <san0558@naver.com>
2022-02-20 06:50:51 +0900

    Update mysql settings.py
    - sqllite대신 mysql docker 사용

--- django_web/config/settings.py
+++ django_web/config/settings.py
@@ -82,10 +82,15 @@ ASGI_APPLICATION = 'config.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "HOST": os.environ.get("RDS_HOST"),
+        "NAME": os.environ.get("RDS_NAME"),
+        "USER": os.environ.get("RDS_USER"),
+        "PASSWORD": os.environ.get("RDS_PASSWORD"),
+        "PORT": os.environ.get("RDS_PORT"),
     }
 }
 
