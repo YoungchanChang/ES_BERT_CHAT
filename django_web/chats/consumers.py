1dacd56249d971fdcf37f46b8632cdf81ca182c0
youngchan <san0558@naver.com>
2022-02-19 21:45:23 +0900

    Update ChatConsumer in consumers.py
    - 컨슈머 객체 업데이트

--- django_web/chats/consumers.py
+++ django_web/chats/consumers.py
@@ -8,6 +8,10 @@ from chats.api_docker import get_mecab_ner
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
+        await self.send(text_data=json.dumps({
+            'message': "안녕!",
+            'messenger': "FriendBot"
+        }))
 
     async def disconnect(self, close_code):
         # Leave room group
@@ -17,14 +21,18 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        messenger = text_data_json['messenger']
 
-
+        print(message)
         # Send message to room group
         await self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'messenger': messenger
         }))
 
+        mecab_ner = str(get_mecab_ner(text_data))
         # Send message to room group
         await self.send(text_data=json.dumps({
-            'message': str(get_mecab_ner(text_data))
+            'message': mecab_ner,
+            'messenger': "FriendBot"
         }))
