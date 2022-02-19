33993313363c9df11197238d0c0a98cceeee68df
youngchan <san0558@naver.com>
2022-02-19 11:29:28 +0900

    Update ChatConsumer in consumers.py
    - 비동기 방식으로 ChatConsumer 객체 업데이트

--- django_web/chats/consumers.py
+++ django_web/chats/consumers.py
@@ -1,18 +1,22 @@
 # chat/consumers.py
 import json
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
 
-    def disconnect(self, close_code):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
         pass
 
-    def receive(self, text_data):
+    # Receive message from WebSocket
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
-        self.send(text_data=json.dumps({
+        # Send message to room group
+        await self.send(text_data=json.dumps({
             'message': message
         }))
