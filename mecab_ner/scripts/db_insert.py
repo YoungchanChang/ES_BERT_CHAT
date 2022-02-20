948eb9fda4386bc2d9402fcb296df25277a1ec78
youngchan <san0558@naver.com>
2022-02-20 10:59:53 +0900

    Update db_insert.py
    - db 삽입 완료

--- mecab_ner/scripts/db_insert.py
+++ mecab_ner/scripts/db_insert.py
@@ -5,26 +5,35 @@ from app.application.service.mecab_generator import MecabGenerator
 ENTITY_DIR_PATH = Path(__file__).resolve().parent.parent.joinpath("datas", 'entities', 'storage')
 print(ENTITY_DIR_PATH)
 m_g = MecabGenerator(storage_path=str(ENTITY_DIR_PATH))
-for path_item in Path(m_g.mecab_path).iterdir():
-    print(path_item)
+# for path_item in Path(m_g.mecab_path).iterdir():
+#     print(path_item)
 
 # init data
-m_g = MecabGenerator(storage_path=ENTITY_DIR_PATH)
+m_g = MecabGenerator(storage_path=str(ENTITY_DIR_PATH))
 
+MecabEntity.drop_table()
 EntityCategoryItem.drop_table()
 EntityCategoryItem.create_table()
+MecabEntity.create_table()
+
+
 for data_item in m_g.gen_all_mecab_category_data(m_g.storage_path, need_parser=True):
     large_category, medium_category, data_dict = data_item
     for data_dict_item in data_dict.keys():
-        # large_category
-        # medium_category
-        # small_category
         small_category = data_dict_item.replace("#", "").strip()
         EntityCategoryItem.create(large_category=large_category,
                                   medium_category=medium_category,
                                   small_category=small_category)
-        print(data_dict_item)
-    print(large_category)
-    print(medium_category)
-    print(data_dict)
 
+
+for data_item in m_g.gen_all_mecab_category_data(m_g.storage_path, need_parser=True):
+    large_category, medium_category, data_dict = data_item
+    for data_dict_item in data_dict.keys():
+        small_category = data_dict_item.replace("#", "").strip()
+        entity_val = EntityCategoryItem.get_or_none(EntityCategoryItem.large_category == large_category,
+                               EntityCategoryItem.medium_category == medium_category,
+                               EntityCategoryItem.small_category == small_category)
+
+        for data_dict_val in data_dict[data_dict_item]:
+            word, mecab_word = data_dict_val
+            MecabEntity.create(word=word, mecab_word=mecab_word, category=entity_val.id)
