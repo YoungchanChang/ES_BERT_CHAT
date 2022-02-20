b0942027cc21617504298bb4210432c6f1bdae0b
youngchan <san0558@naver.com>
2022-02-20 14:11:55 +0900

    Add IntentCategoryItemAdmin in admin.py
    - 관리자 페이지에서 IntentCategoryItemAdmin 클래스 분리

--- django_web/mecab_ner/admin.py
+++ django_web/mecab_ner/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-@admin.register(models.EntityCategoryItem, models.IntentCategoryItem)
-class ItemAdmin(admin.ModelAdmin):
+@admin.register(models.EntityCategoryItem)
+class EntityCategoryItemAdmin(admin.ModelAdmin):
 
     """ Item Admin Definition """
 
@@ -19,8 +19,63 @@ class ItemAdmin(admin.ModelAdmin):
         return obj.entity.count()
 
 
+@admin.register(models.IntentCategoryItem)
+class IntentCategoryItemAdmin(admin.ModelAdmin):
+
+    """ Item Admin Definition """
+
+    list_display = ("small_category", "medium_category", "large_category", "used_by")
+
+    list_filter = (
+        "small_category",
+        "medium_category",
+        "large_category",
+    )
+
+    def used_by(self, obj):
+        return obj.intent.count()
+
+
+
 @admin.register(models.MecabEntity)
 class MecabEntityAdmin(admin.ModelAdmin):
+
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("word",)}
+        ),
+        ("Last Details", {"fields": ("category",)}),
+    )
+    list_display = (
+        "word",
+        "mecab_word",
+        "show_small_category",
+        "show_medium_category",
+        'show_large_category',
+    )
+
+    def show_small_category(self, obj):
+        return obj.category.small_category
+
+    def show_medium_category(self, obj):
+        return obj.category.medium_category
+
+    def show_large_category(self, obj):
+        return obj.category.large_category
+
+    search_fields = ("word", )
+    raw_id_fields = ("category",)
+
+    list_filter = (
+        "category__large_category",
+        "category__medium_category",
+        "category__small_category",
+    )
+
+
+@admin.register(models.MecabIntent)
+class MecabIntentAdmin(admin.ModelAdmin):
 
     fieldsets = (
         (
