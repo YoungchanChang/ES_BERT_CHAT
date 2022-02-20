82a1d32d99f6b1db91af04eef55e285d78f41354
youngchan <san0558@naver.com>
2022-02-20 16:19:02 +0900

    Update write_txt in data_reader.py
    - 데이터 삽입시 마지막 줄 엔터 관련 로직 업데이트

--- mecab_ner/app/utility/data_reader.py
+++ mecab_ner/app/utility/data_reader.py
@@ -1,3 +1,4 @@
+from typing import List
 
 
 class DataReader:
@@ -9,11 +10,14 @@ class DataReader:
             return txt_list
 
     @staticmethod
-    def write_txt(data_path, txt_list, is_sort=False):
+    def write_txt(data_path: str, txt_list: List, is_sort=False):
         if is_sort:
             txt_list = sorted(list(txt_list), key=len, reverse=True)
 
         with open(data_path, "a", encoding='UTF8') as file:
-            for txt_item in txt_list:
-                data = str(txt_item) + "\n"
+            for idx, txt_item in enumerate(txt_list):
+                if len(txt_list) == idx+1:
+                    data = str(txt_item)
+                else:
+                    data = str(txt_item) + "\n"
                 file.write(data)
