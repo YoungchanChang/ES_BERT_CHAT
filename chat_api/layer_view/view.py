501e6322cfc1f2c1965e47e607b266b6ecc07b83
youngchan <san0558@naver.com>
2022-02-21 13:30:39 +0900

    Update request_from_chat_middleware in chat_api
    - 요청에 템플릿 응답할 수 있도록 변경

--- chat_api/layer_view/view.py
+++ chat_api/layer_view/view.py
@@ -14,14 +14,16 @@ router = APIRouter(
 
 
 @router.post("/response_api")
-async def request_chat_middleware(chat_api_req: ChatApiRequest):
+async def request_from_chat_middleware(chat_api_req: ChatApiRequest):
     try:
         middle_m_n_a = chat_api_req.m_n_a
         t_r_d = TemplateRequestData(main_category=middle_m_n_a.main_category, entity_medium_category=middle_m_n_a.entity_medium_category,
                            entity=middle_m_n_a.entity, intent_small_category=middle_m_n_a.intent_small_category)
-        get_template_data(t_r_d)
-        c_a_res = ChatApiResponse(api_template="나도 치킨이 좋아요", api_server="basic_template", system_response_time="2022-02-21T07:03:52.716025")
+        api_template = get_template_data(t_r_d)
+        if api_template:
+            c_a_res = ChatApiResponse(api_template=api_template, api_server="basic_template", system_response_time="2022-02-21T07:03:52.716025")
+            return jsonable_encoder(c_a_res)
     except ValidationError as e:
         return e.json()
+    return jsonable_encoder(False)
 
-    return jsonable_encoder(c_a_res)
