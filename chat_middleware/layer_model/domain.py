b3b1c26a2449f8a6b8bbadb768a409e2b725dbe3
youngchan <san0558@naver.com>
2022-02-21 14:26:53 +0900

    Add Response in domain.py
    - 응답 객체 상속받을 수 있도록 변경

--- chat_middleware/layer_model/domain.py
+++ chat_middleware/layer_model/domain.py
@@ -8,16 +8,22 @@ class Request(BaseModel):
     """ 사용자 요청 정보 """
 
     user_sentence: str
-    user_request_time: datetime
     user_ip: str
+    user_request_time: datetime
     request_get_time: datetime = datetime.now().isoformat(timespec='microseconds')
 
 
-class ChatMiddlewareResponse(BaseModel):
+class Response(BaseModel):
+
+    """ 시스템 응답 정보 """
+
+    system_response_time: datetime = datetime.now().isoformat(timespec='microseconds')
+
+
+class ChatMiddlewareResponse(Response):
     """ 챗미들웨어 서버에서 보내는 응답 """
 
     bot_response: str
-    bot_response_time: datetime = datetime.now().isoformat(timespec='microseconds')
 
 
 class MecabNerAttribute(BaseModel):
@@ -32,28 +38,27 @@ class MecabNerAttribute(BaseModel):
     bert_confirm: bool
 
 
-class MecabNerResponse(BaseModel):
+class MecabNerResponse(Response):
 
     """ MecabNer에서 받는 응답 """
 
     user_sentence: str
     is_atomic: bool
     sentence_attributes: List[MecabNerAttribute]
-    system_response_time: datetime = datetime.now().isoformat(timespec='microseconds')
 
 
 class ChatApiRequest(BaseModel):
 
     """ ChatApi 서버에 보내는 요청 """
 
-    m_n_a: MecabNerAttribute
     req: Request
+    is_atomic: bool
+    sentence_attributes: List[MecabNerAttribute]
 
 
-class ChatApiResponse(BaseModel):
+class ChatApiResponse(Response):
 
     """ ChatApi 서버에 보내는 응답 """
 
     api_response: str
     api_server: str
-    system_response_time: datetime = datetime.now().isoformat(timespec='microseconds')
