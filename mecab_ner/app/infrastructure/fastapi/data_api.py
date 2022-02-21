051d939f7bb4219cf1b8700c7aa49ef6b67d12ca
youngchan <san0558@naver.com>
2022-02-21 20:25:37 +0900

    Add data_api.py
    - 데이터 api 추가

---
+++ mecab_ner/app/infrastructure/fastapi/data_api.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter
+from fastapi.encoders import jsonable_encoder
+
+from app.controller.service.mecab_controller import MecabDataController
+from app.domain.entity import CategoryItem, MecabWord
+
+router = APIRouter(
+    prefix="/mecab_data",
+    tags=["mecab_ner"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.post("/create_index")
+async def create_index(category_item: CategoryItem):
+    is_index_created = MecabDataController.create_index(category_item)
+    return_json = {"result": is_index_created}
+    return jsonable_encoder(return_json)
+
+
+@router.post("/insert_data")
+async def insert_data(mecab_word: MecabWord):
+    is_index_created = MecabDataController.insert_data(mecab_word)
+    return_json = {"result": is_index_created}
+    return jsonable_encoder(return_json)
