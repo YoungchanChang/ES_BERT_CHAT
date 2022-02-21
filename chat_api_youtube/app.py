47acd7b413b6140096efae0b3fbb800b62029e26
youngchan <san0558@naver.com>
2022-02-21 14:39:21 +0900

    Update chat_api_youtube
    - 챗 유튜브 업데이트

--- chat_api_youtube/app.py
+++ chat_api_youtube/app.py
@@ -12,5 +12,5 @@ app.include_router(view.router)
 
 if __name__ == "__main__":
     uvicorn.run(
-        "app:app", host="0.0.0.0", port=5220, reload=True
+        "app:app", host="0.0.0.0", port=5210, reload=True
     )
--- chat_api_youtube/layer_view/view.py
+++ chat_api_youtube/layer_view/view.py
@@ -2,8 +2,7 @@ from fastapi import APIRouter
 from fastapi.encoders import jsonable_encoder
 from pydantic import ValidationError
 
-from chat_api_mrc.layer_model.domain import ChatApiResponse
-from chat_middleware.layer_model.domain import ChatMiddlewareResponse, Request
+from chat_api_youtube.layer_model.domain import ChatApiRequest, ChatApiResponse
 
 router = APIRouter(
     prefix="/chat_api_youtube",
@@ -12,8 +11,8 @@ router = APIRouter(
 )
 
 
-@router.post("/response_mrc")
-async def request_from_chat_api(chat_api_req: Request):
+@router.post("/response_youtube")
+async def request_from_chat_api(chat_api_req: ChatApiRequest):
     try:
         c_a_res = ChatApiResponse(api_template="나도 치킨이 좋아요", api_server="basic_template", system_response_time="2022-02-21T07:03:52.716025")
     except ValidationError as e:
