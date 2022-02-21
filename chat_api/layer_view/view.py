73016621cee71abbac6a458e63a57b58f16bd978
youngchan <san0558@naver.com>
2022-02-21 13:58:35 +0900

    Update view.py in chat_api
    - 카테고리에 따른 다른 api서버 이용하도록 변경

--- chat_api/layer_view/view.py
+++ chat_api/layer_view/view.py
@@ -1,9 +1,11 @@
+import json
 from fastapi import APIRouter
 from fastapi.encoders import jsonable_encoder
 from pydantic import ValidationError
 
 from chat_api.layer_control.control import get_template_data
 from chat_api.layer_model.domain import ChatApiRequest, ChatApiResponse, TemplateRequestData
+from chat_api.utility.api_endpoint import get_youtube_api_response, get_mrc_api_response
 from chat_middleware.layer_model.domain import ChatMiddlewareResponse, Request
 
 router = APIRouter(
@@ -19,11 +21,25 @@ async def request_from_chat_middleware(chat_api_req: ChatApiRequest):
         middle_m_n_a = chat_api_req.m_n_a
         t_r_d = TemplateRequestData(main_category=middle_m_n_a.main_category, entity_medium_category=middle_m_n_a.entity_medium_category,
                            entity=middle_m_n_a.entity, intent_small_category=middle_m_n_a.intent_small_category)
+
+        if middle_m_n_a.main_category == "music":
+            youtube_response = get_youtube_api_response(chat_api_req.json())
+            c_a_res = ChatApiResponse(api_response="유튜브 음악입니다", api_server="youtube_template")
+            return jsonable_encoder(c_a_res)
+
+        if middle_m_n_a.main_category == "question":
+            mrc_response = get_mrc_api_response(chat_api_req.json())
+            c_a_res = ChatApiResponse(api_response="MRC 응답입니다.", api_server="mrc_template")
+            return jsonable_encoder(c_a_res)
+
         api_template = get_template_data(t_r_d)
         if api_template:
-            c_a_res = ChatApiResponse(api_response=api_template, api_server="basic_template", system_response_time="2022-02-21T07:03:52.716025")
+            c_a_res = ChatApiResponse(api_response=api_template, api_server="basic_template")
             return jsonable_encoder(c_a_res)
     except ValidationError as e:
         return e.json()
+    except Exception as e:
+        print(e)
+        return jsonable_encoder(False)
     return jsonable_encoder(False)
 
