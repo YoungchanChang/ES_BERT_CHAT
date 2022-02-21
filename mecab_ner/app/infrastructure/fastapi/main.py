8078472579373bc4f33ea7c000d0bf37a9cc4e0c
youngchan <san0558@naver.com>
2022-02-21 20:26:28 +0900

    Update main.py
    - 라우터로 변경

--- mecab_ner/app/infrastructure/fastapi/main.py
+++ mecab_ner/app/infrastructure/fastapi/main.py
@@ -1,46 +1,15 @@
 import uvicorn
-from fastapi import FastAPI
-
-from dotenv import load_dotenv
-
+from fastapi import FastAPI, APIRouter
 from app.domain.entity import Query, CategoryItem, MecabWord
+from app.infrastructure.fastapi import mecab_api, data_api
 from app.utility.docker_net import get_entity_intent_answer
-
-load_dotenv()
 from app.controller.service.mecab_controller import get_data, MecabDataController
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 
-from fastapi.encoders import jsonable_encoder
-
-
-@app.post("/mecab_entity")
-async def create_item(sentence: Query):
-    return_json = {"answer": jsonable_encoder(get_data(sentence=sentence.sentence))}
-    return jsonable_encoder(return_json)
-
-
-@app.post("/mecab_entity/bert_chat")
-async def bert_chat(sentence: Query):
-    return_json = {"answer": jsonable_encoder(get_data(sentence=sentence.sentence))}
-    return_val = get_entity_intent_answer(return_json)
-    return_json = {"answer": return_val}
-    return jsonable_encoder(return_json)
-
-
-@app.post("/mecab_entity/create_index")
-async def create_index(category_item: CategoryItem):
-    is_index_created = MecabDataController.create_index(category_item)
-    return_json = {"answer": is_index_created}
-    return jsonable_encoder(return_json)
-
-
-@app.post("/mecab_entity/insert_data")
-async def insert_data(mecab_word: MecabWord):
-    is_index_created = MecabDataController.insert_data(mecab_word)
-    return_json = {"answer": is_index_created}
-    return jsonable_encoder(return_json)
-
+app.include_router(mecab_api.router)
+app.include_router(data_api.router)
 
 
 if __name__ == "__main__":
