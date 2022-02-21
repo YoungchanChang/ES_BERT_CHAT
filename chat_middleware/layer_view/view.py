fdd462f590bc11a06553d2990baf20cdade37280
youngchan <san0558@naver.com>
2022-02-21 16:06:26 +0900

    Add request_chat_api in chat_middleware
    - chat_api 테스트

--- chat_middleware/layer_view/view.py
+++ chat_middleware/layer_view/view.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter
 from fastapi.encoders import jsonable_encoder
 from pydantic import ValidationError
 
-from chat_middleware.layer_model.domain import ChatMiddlewareResponse, Request
+from chat_middleware.layer_model.domain import ChatMiddlewareResponse, Request, ChatApiRequest, ChatApiResponse
+from chat_middleware.utility.api_endpoint import get_chat_api_response
 
 router = APIRouter(
     prefix="/chat_middleware",
@@ -19,3 +20,14 @@ async def request_from_django_web(django_req: Request):
         return e.json()
 
     return jsonable_encoder(c_m_res)
+
+
+@router.post("/test_chat_api")
+async def request_chat_api(chat_api_req: ChatApiRequest):
+    try:
+        chat_api_response = get_chat_api_response(chat_api_req.json())
+        c_a_res = ChatApiResponse(api_response=chat_api_response, api_server="chat_api_template")
+        return jsonable_encoder(c_a_res)
+
+    except ValidationError as e:
+        return e.json()
