377e4b99288a7b79025baf83096dd25e2d8d3e7e
youngchan <san0558@naver.com>
2022-02-21 15:11:01 +0900

    Update control.py in chat_api_mrc
    - 기본 응답 추가

---
+++ chat_api_mrc/layer_control/control.py
@@ -0,0 +1,8 @@
+from typing import List
+
+from chat_api_mrc.layer_model.domain import MecabNerAttribute
+
+
+def get_mrc(sentence_attributes: List[MecabNerAttribute]):
+
+    return "잘 모르겠어요. 제가 공부해서 꼭 알려드릴께요!"
