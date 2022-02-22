82a0f095eb1a2ec268816fb640876fd817975730
youngchan <san0558@naver.com>
2022-02-23 06:06:02 +0900

    Update settings.py
    - 챗 환경설정 파일 변경

--- chat_core/settings.py
+++ chat_core/settings.py
@@ -1,3 +1,3 @@
 from pathlib import Path
 
-chat_log_ini = Path(__file__).resolve().parent.parent.joinpath('chat_log', 'chat_log.ini')
+chat_log_path = Path(__file__).resolve().parent.parent.joinpath('chat_log')
