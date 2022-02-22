3508013ed1b382d72d53d6812943cd59302fb279
youngchan <san0558@naver.com>
2022-02-23 04:41:58 +0900

    Update django_web
    - django_web 테스트 api 생성

--- django_web/chats/api_docker.py
+++ django_web/chats/api_docker.py
@@ -1,11 +1,15 @@
 import os
 import json
+from datetime import datetime
+
 import requests
 
 
 def get_bot_response(text_data):
     json_data = {
-        "sentence": text_data
+        "user_sentence": text_data,
+        "user_ip" : "127.0.0.1",
+        "user_request_time": datetime.now().isoformat(timespec='microseconds')
     }
     utility_answer = requests.post(
         os.getenv("bot_response"), headers={'content-type': 'application/json'}, data=json.dumps(json_data)
