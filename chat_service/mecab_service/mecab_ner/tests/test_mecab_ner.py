c92c548101ce6d4113b3b3e9a5e4459a1fcbc422
youngchan <san0558@naver.com>
2022-02-23 06:55:09 +0900

    Move chat api app to chat_api_service
    - 챗 api 앱을 챗 api 서비스로 이동

--- chat_service/mecab_service/mecab_ner/tests/test_mecab_ner.py
+++ chat_service/mecab_service/mecab_ner/tests/test_mecab_ner.py
@@ -54,7 +54,7 @@ def test_gen_mecab_token_type_feature():
 
 
 def test_read_category():
-    storage_path = "/Users/youngchan/Desktop/ES_BERT_CHAT/mecab_ner/datas/entities/storage"
+    storage_path = "/chat_service/mecab_service/mecab_ner/datas/entities/storage"
 
     # clear data
     m_g = MecabGenerator(storage_path=storage_path)
@@ -76,7 +76,7 @@ def test_read_category():
 
 
 def test_read_intent_category():
-    storage_path = "/Users/youngchan/Desktop/ES_BERT_CHAT/mecab_ner/datas/intents/storage"
+    storage_path = "/chat_service/mecab_service/mecab_ner/datas/intents/storage"
 
     # clear data
     m_g = MecabGenerator(storage_path=storage_path)
@@ -101,7 +101,7 @@ def test_storage_same():
 
     """ mecab 저장 데이터와 storage 데이터의 길이가 같은지 확인 """
 
-    storage_path = "/Users/youngchan/Desktop/ES_BERT_CHAT/mecab_ner/datas/entities/storage"
+    storage_path = "/chat_service/mecab_service/mecab_ner/datas/entities/storage"
 
     for path_item in Path(storage_path).iterdir():
         stroage_data_len = len(DataReader.read_txt(path_item))
@@ -113,7 +113,7 @@ def test_storage_same():
 def test_mecab_ner():
     """간단한 문장에서 엔티티 추출"""
     sentence = "나는 딸기가 먹고 싶어"
-    entity_storage_path = "/Users/youngchan/Desktop/ES_BERT_CHAT/mecab_ner/datas/entities/mecab_storage"
+    entity_storage_path = "/chat_service/mecab_service/mecab_ner/datas/entities/mecab_storage"
     mecab_ner = MeCabNer(storage_mecab_path=entity_storage_path)
 
     for mecab_category_item in mecab_ner.get_entities(sentence=sentence):
@@ -123,7 +123,7 @@ def test_mecab_ner():
         assert mecab_category_item.small_category == "#과일"
         assert mecab_category_item.entity == "딸기"
 
-    intent_storage_path = "/Users/youngchan/Desktop/ES_BERT_CHAT/mecab_ner/datas/intents/mecab_storage"
+    intent_storage_path = "/chat_service/mecab_service/mecab_ner/datas/intents/mecab_storage"
     mecab_ner_intents = MeCabNer(storage_mecab_path=intent_storage_path)
 
     for mecab_intent_category_item in mecab_ner_intents.get_entities(sentence=sentence):
@@ -135,7 +135,7 @@ def test_mecab_ner():
 
 def test_infer_mecab_ner():
     sentence = "나는 신촌 딸기가 먹고 싶어"
-    entity_storage_path = "/Users/youngchan/Desktop/ES_BERT_CHAT/mecab_ner/datas/entities/mecab_storage"
+    entity_storage_path = "/chat_service/mecab_service/mecab_ner/datas/entities/mecab_storage"
     mecab_ner = MeCabNer(storage_mecab_path=entity_storage_path)
 
     for mecab_category_item in mecab_ner.get_entities(sentence=sentence, status=MeCabNer.INFER_FORWARD):
@@ -147,7 +147,7 @@ def test_infer_mecab_ner():
 
 def test_infer_backward():
     sentence = "나는 라떼 한 잔이 먹고 싶어"
-    entity_storage_path = "/Users/youngchan/Desktop/ES_BERT_CHAT/mecab_ner/datas/entities/mecab_storage"
+    entity_storage_path = "/chat_service/mecab_service/mecab_ner/datas/entities/mecab_storage"
     mecab_ner = MeCabNer(storage_mecab_path=entity_storage_path)
 
     for mecab_category_item in mecab_ner.get_entities(sentence=sentence, status=MeCabNer.INFER_BACKWARD):
@@ -159,7 +159,7 @@ def test_infer_backward():
 
 def test_filter_entity():
     sentence = "나는 스윗한 딸기 치킨 먹고 싶어 사두감도 먹고 싶어"
-    entity_storage_path = "/Users/youngchan/Desktop/ES_BERT_CHAT/mecab_ner/datas/entities/mecab_storage"
+    entity_storage_path = "/chat_service/mecab_service/mecab_ner/datas/entities/mecab_storage"
     mecab_ner = MeCabNer(storage_mecab_path=entity_storage_path)
     entity_list = mecab_ner.gen_integrated_entities(sentence, status=MeCabNer.INFER_FORWARD)
     entity_list = list(entity_list)
@@ -174,7 +174,7 @@ def test_filter_entity():
     assert entity_list[1].entity == "사두감"
 
     sentence = "나는 딸기랑 감이 먹고 싶어"
-    entity_storage_path = "/Users/youngchan/Desktop/ES_BERT_CHAT/mecab_ner/datas/entities/mecab_storage"
+    entity_storage_path = "/chat_service/mecab_service/mecab_ner/datas/entities/mecab_storage"
     mecab_ner = MeCabNer(storage_mecab_path=entity_storage_path)
     entity_list = mecab_ner.gen_integrated_entities(sentence, status=MeCabNer.INFER_FORWARD)
     entity_list = list(entity_list)
@@ -191,13 +191,13 @@ def test_filter_entity():
 
 def test_word_sense_disambiguation():
     sentence = "나는 차가 좋다"
-    entity_storage_path = "/Users/youngchan/Desktop/ES_BERT_CHAT/mecab_ner/datas/entities/mecab_storage"
+    entity_storage_path = "/chat_service/mecab_service/mecab_ner/datas/entities/mecab_storage"
     mecab_ner = MeCabNer(storage_mecab_path=entity_storage_path)
     entity_list = mecab_ner.gen_integrated_entities(sentence, status=MeCabNer.INFER_FORWARD)
     entity_list = list(entity_list)
     assert len(entity_list) == 2
 
-    entity_storage_path = "/Users/youngchan/Desktop/ES_BERT_CHAT/mecab_ner/datas/intents/mecab_storage"
+    entity_storage_path = "/chat_service/mecab_service/mecab_ner/datas/intents/mecab_storage"
     mecab_ner = MeCabNer(storage_mecab_path=entity_storage_path)
     intent_list = mecab_ner.gen_integrated_entities(sentence, status=MeCabNer.ENTITY)
     intent_list = list(intent_list)
