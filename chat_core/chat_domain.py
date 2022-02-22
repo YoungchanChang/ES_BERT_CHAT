4ec072d4ff188aa37278243247a3d1c34564f740
youngchan <san0558@naver.com>
2022-02-23 05:28:32 +0900

    Add chat_core
    - 채팅 코어 클래스 생성

---
+++ chat_core/chat_domain.py
@@ -0,0 +1,52 @@
+from pydantic import BaseModel, validator
+from datetime import datetime, date
+from typing import List, Optional
+
+
+class UserRequest(BaseModel):
+
+    """ 사용자 요청 정보 """
+
+    user_sentence: str
+    user_ip: str
+    user_request_time: datetime
+
+
+class MecabNerAttribute(BaseModel):
+
+    """ MecabNer 추출 정보 """
+
+    category_sentence: str
+    main_category: str
+    entity: str
+    entity_medium_category: str
+    entity_small_category: str
+    intent: str
+    intent_medium_category: str
+    intent_small_category: str
+    bert_confirm: bool
+
+
+class ChatApiRequest(BaseModel):
+
+    """ ChatApi 서버에 보내는 요청 """
+
+    user_info: UserRequest
+    sentence_attributes: List[MecabNerAttribute]
+
+
+class ChatApiResponse(BaseModel):
+
+    """ ChatApi 서버에 보내는 응답 """
+
+    api_response: str
+    api_server: str
+
+
+class TemplateRequestData(BaseModel):
+    """ 템플릿에 요청 정보 """
+
+    main_category: str
+    entity_medium_category: str
+    entity: str
+    intent_small_category: str
---
+++ chat_core/settings.py
@@ -0,0 +1,3 @@
+from pathlib import Path
+
+chat_log_ini = Path(__file__).resolve().parent.parent.joinpath('chat_log', 'chat_log.ini')
