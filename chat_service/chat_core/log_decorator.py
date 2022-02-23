466f0a0c59ac4ae99a4ca15fca58454a7aa9019c
youngchan <san0558@naver.com>
2022-02-24 07:59:35 +0900

    Add log_decorator
    - 인스턴스 제너레이터타입 검증 추가

--- chat_service/chat_core/log_decorator.py
+++ chat_service/chat_core/log_decorator.py
@@ -2,6 +2,7 @@ import functools
 import logging
 import time
 import sys, traceback
+import types
 
 
 def log_basic(logger):
@@ -13,11 +14,15 @@ def log_basic(logger):
             try:
                 func_result = func(*args, **kwargs)
 
+                return_func_result = func_result
+                if isinstance(func_result, types.GeneratorType):
+                    return_func_result = list(func_result)
+
                 debug_info = {"name": __name__, "method_name": func.__name__, "args": args, "kwargs": kwargs,
-                              "return": func_result,
+                              "return": return_func_result,
                               "total_time": round(time.perf_counter() - st, 3)}
                 logger.debug(debug_info)
-                return func_result
+                return return_func_result
 
             except Exception as e:
 
