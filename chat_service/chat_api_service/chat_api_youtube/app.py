f627b3756c9b2c15b275068c779f39b7bf9565ba
youngchan <san0558@naver.com>
2022-02-24 15:01:47 +0900

    Update chat_api_service
    - port number changed

--- chat_service/chat_api_service/chat_api_youtube/app.py
+++ chat_service/chat_api_service/chat_api_youtube/app.py
@@ -19,5 +19,5 @@ app.include_router(view.router)
 
 if __name__ == "__main__":
     uvicorn.run(
-        "app:app", host="0.0.0.0", port=5210, reload=True
+        "app:app", host="0.0.0.0", port=5211, reload=True
     )
