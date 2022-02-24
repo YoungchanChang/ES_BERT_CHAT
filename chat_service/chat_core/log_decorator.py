2deb533ea29e6a742f1585a104d2bfa53d23e532
youngchan <san0558@naver.com>
2022-02-24 09:17:26 +0900

    Update log_decorator.py
    - 로그 데코레이터 path부분 업데이트

--- chat_service/chat_core/log_decorator.py
+++ chat_service/chat_core/log_decorator.py
@@ -1,11 +1,9 @@
 import functools
-import logging
 import time
-import sys, traceback
+import traceback
 import types
 
-
-def log_basic(logger):
+def log_basic(logger, path=None):
     def log_decorated(func):
         @functools.wraps(func)
         def log_wrapper(*args, **kwargs):
@@ -18,7 +16,7 @@ def log_basic(logger):
                 if isinstance(func_result, types.GeneratorType):
                     return_func_result = list(func_result)
 
-                debug_info = {"name": __name__, "method_name": func.__name__, "args": args, "kwargs": kwargs,
+                debug_info = {"path": path, "method_name": func.__name__, "args": args, "kwargs": kwargs,
                               "return": return_func_result,
                               "total_time": round(time.perf_counter() - st, 3)}
                 logger.debug(debug_info)
@@ -26,7 +24,7 @@ def log_basic(logger):
 
             except Exception as e:
 
-                exception_info = {"name": __name__, "method_name": func.__name__, "args": args, "kwargs": kwargs,
+                exception_info = {"path": path,  "method_name": func.__name__, "args": args, "kwargs": kwargs,
                                   "exception_class": e,
                                   "exception_reason": traceback.format_exc(),
                                   "total_time": round(time.perf_counter() - st, 3)}
