1cd6d4b3e55670beb3ba044d80fb826983b8c7ba
youngchan <san0558@naver.com>
2022-02-24 17:02:49 +0900

    Update settings.py in chat_core
    - 환경설정에 경로 추가

--- chat_service/chat_core/settings.py
+++ chat_service/chat_core/settings.py
@@ -1,4 +1,14 @@
 from pathlib import Path
+import platform
 
 chat_log_path = Path(__file__).resolve().parent.parent.joinpath('chat_log')
 
+chat_api_youtube = "chat_api_youtube"
+chat_api_mrc = "chat_api_mrc"
+if platform.system() == "Darwin":
+    chat_api_mrc = "localhost"
+    chat_api_youtube = "localhost"
+
+
+youtube_response_url = f"http://{chat_api_youtube}:5210/chat_api_youtube/response_youtube"
+mrc_response_url = f"http://{chat_api_mrc}:5220/chat_api_mrc/response_mrc"
