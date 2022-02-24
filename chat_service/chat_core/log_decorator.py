dc45ace484f7513884729d70adecd9e1fecaf9a3
youngchan <san0558@naver.com>
2022-02-24 10:30:31 +0900

    Update log_decorator
    - method_name을 method로 변경

--- chat_service/chat_core/log_decorator.py
+++ chat_service/chat_core/log_decorator.py
@@ -16,7 +16,7 @@ def log_basic(logger, path=None):
                 if isinstance(func_result, types.GeneratorType):
                     return_func_result = list(func_result)
 
-                debug_info = {"path": path, "method_name": func.__name__, "args": args, "kwargs": kwargs,
+                debug_info = {"path": path, "method": func.__name__, "args": args, "kwargs": kwargs,
                               "return": return_func_result,
                               "total_time": round(time.perf_counter() - st, 3)}
                 logger.debug(debug_info)
@@ -24,7 +24,7 @@ def log_basic(logger, path=None):
 
             except Exception as e:
 
-                exception_info = {"path": path,  "method_name": func.__name__, "args": args, "kwargs": kwargs,
+                exception_info = {"path": path,  "method": func.__name__, "args": args, "kwargs": kwargs,
                                   "exception_class": e,
                                   "exception_reason": traceback.format_exc(),
                                   "total_time": round(time.perf_counter() - st, 3)}
