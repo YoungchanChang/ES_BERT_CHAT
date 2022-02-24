141a42dab29c8f7f1baec37ad4c996e569972cd6
youngchan <san0558@naver.com>
2022-02-24 09:48:19 +0900

    Add log in view.py
    - 뷰 함수에 로그 추가

--- chat_service/chat_api_service/chat_api_youtube/layer_view/view.py
+++ chat_service/chat_api_service/chat_api_youtube/layer_view/view.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter
+import logging.config
+
+from fastapi import APIRouter, Request
 from fastapi.encoders import jsonable_encoder
 from pydantic import ValidationError
 
 from chat_service.chat_api_service.chat_api_youtube.layer_control.control import get_youtube_music
-from chat_service.chat_api_service.chat_api_youtube.layer_model.domain import ChatApiRequest, ChatApiResponse
 from chat_service.chat_api_service.chat_api_youtube.utility.custom_error import NoMusicData
+from chat_core.chat_domain import ChatApiRequest, ChatApiResponse
+
+logger = logging.getLogger('simple_log')
 
 router = APIRouter(
     prefix="/chat_api_youtube",
@@ -14,17 +18,23 @@ router = APIRouter(
 
 
 @router.post("/response_youtube")
-async def request_from_chat_api(chat_api_req: ChatApiRequest):
+async def request_from_chat_api(chat_api_req: ChatApiRequest, request: Request):
 
     try:
         youtube_res = get_youtube_music(chat_api_req.sentence_attributes)
         c_a_res = ChatApiResponse(api_response=youtube_res, api_server="youtube_template")
+        logger.info(
+            {'status': 'success', 'user_ip': request.client.host, "request_path": request.url.path, "return": c_a_res})
         return jsonable_encoder(c_a_res)
 
-    except ValidationError as e:
-        return e.json()
-    except NoMusicData as e:
-            ...
+    except ValidationError as ve:
+        logger.error(
+            {'status': 'fail', 'user_ip': request.client.host, "request_path": request.url.path, "message": ve})
+        return ve.json()
+    except NoMusicData as me:
+        logger.warning(
+            {'status': 'fail', 'user_ip': request.client.host, "request_path": request.url.path, "message": me,
+             "music_request": chat_api_req.sentence_attributes})
     c_a_res = ChatApiResponse(api_response="음악을 준비중이예요. 조금만 기다려주세요.", api_server="basic_template")
     return jsonable_encoder(c_a_res)
 
