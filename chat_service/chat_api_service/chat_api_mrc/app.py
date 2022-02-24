d164c825ec8abb5edf2976e19a4b81e2e5160e19
youngchan <san0558@naver.com>
2022-02-24 09:19:32 +0900

    Update app.py in api_mrc
    - app.py에 로그 파일 설정 추가

--- chat_service/chat_api_service/chat_api_mrc/app.py
+++ chat_service/chat_api_service/chat_api_mrc/app.py
@@ -1,8 +1,18 @@
+import logging.config
+
 import uvicorn
 from fastapi import FastAPI
 from dotenv import load_dotenv
 
 from layer_view import view
+
+from chat_core.chat_log_config import config_basic
+from chat_core.settings import chat_log_path
+
+config_basic['handlers']['access']['filename'] = str(chat_log_path.joinpath('chat_api_mrc', 'access', 'access.log'))
+config_basic['handlers']['error']['filename'] = str(chat_log_path.joinpath('chat_api_mrc', 'error', 'error.log'))
+logging.config.dictConfig(config_basic)
+logger = logging.getLogger(__name__)
 
 load_dotenv()
 
