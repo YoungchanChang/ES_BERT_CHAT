4a102e50365b64906656456d87c99ab0f5b0db2e
youngchan <san0558@naver.com>
2022-02-24 09:24:46 +0900

    Add app in api_youtube
    - 앱 로그 환경설정 추가

--- chat_service/chat_api_service/chat_api_youtube/app.py
+++ chat_service/chat_api_service/chat_api_youtube/app.py
@@ -1,8 +1,19 @@
+import logging.config
+
 import uvicorn
 from fastapi import FastAPI
 from dotenv import load_dotenv
 
 from chat_service.chat_api_service.chat_api_youtube.layer_view import view
+
+from chat_core.chat_log_config import config_basic
+from chat_core.settings import chat_log_path
+
+
+config_basic['handlers']['access']['filename'] = str(chat_log_path.joinpath('chat_api_mrc', 'access', 'access.log'))
+config_basic['handlers']['error']['filename'] = str(chat_log_path.joinpath('chat_api_mrc', 'error', 'error.log'))
+logging.config.dictConfig(config_basic)
+logger = logging.getLogger(__name__)
 
 load_dotenv()
 
