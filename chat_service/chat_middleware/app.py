da1359e5025020505b93587e205335b6d23a72a2
youngchan <san0558@naver.com>
2022-02-24 20:55:23 +0900

    Update app.py in chat_middleware
    - 챗 미들웨어 예외처리 추가

--- chat_service/chat_middleware/app.py
+++ chat_service/chat_middleware/app.py
@@ -1,14 +1,30 @@
-import uvicorn
-from fastapi import FastAPI
-from dotenv import load_dotenv
+import logging.config
 
-from chat_service.chat_middleware.layer_view import view
+import uvicorn
+from fastapi import FastAPI, Request
+from dotenv import load_dotenv
+from fastapi.responses import JSONResponse
+
+from layer_view import view
+
+from config.settings import config_basic
+
+logging.config.dictConfig(config_basic)
+logger = logging.getLogger(__name__)
 
 load_dotenv()
 
 app = FastAPI()
 
 app.include_router(view.router)
+
+
+@app.exception_handler(Exception)
+async def exception_general_handler(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=418,
+        content={"message": f"Nope wrong Ask"},
+    )
 
 if __name__ == "__main__":
     uvicorn.run(
