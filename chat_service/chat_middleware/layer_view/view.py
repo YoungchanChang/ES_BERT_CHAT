bbe454857f159584e5832a105ee715f8f23e4e4c
youngchan <san0558@naver.com>
2022-02-24 22:07:35 +0900

    Update view.py in chat_middleware
    - 챗 미들웨어 예외처리 및 로그 처리 코드 추가

--- chat_service/chat_middleware/layer_view/view.py
+++ chat_service/chat_middleware/layer_view/view.py
@@ -1,11 +1,20 @@
+import logging.config
 import json
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, HTTPException
 from fastapi.encoders import jsonable_encoder
 from pydantic import ValidationError
 
-from chat_service.chat_middleware.layer_model.domain import ChatMiddlewareResponse, Request, ChatApiRequest, ChatApiResponse
-from chat_service.chat_middleware.utility.api_endpoint import get_chat_api_response, get_mecab_ner_response
+from chat_core.chat_domain import ChatApiRequest, ChatApiResponse, UserRequest
+from layer_model.domain import SimpleRequest, BotResponse
+from utility.api_endpoint import get_chat_api_response, get_mecab_ner_response
+
+from config.settings import config_basic
+from utility.custom_error import DockerNetworkException
+
+logging.config.dictConfig(config_basic)
+logger = logging.getLogger('simple_log')
+
 
 router = APIRouter(
     prefix="/chat_middleware",
@@ -15,14 +24,27 @@ router = APIRouter(
 
 
 @router.post("/response_middleware")
-async def request_from_django_web(django_req: Request):
+async def request_from_django_web(django_req: SimpleRequest, request: Request) -> BotResponse:
+
     try:
-        mecab_ner_response = get_mecab_ner_response(django_req.json())
+        print(django_req)
+        print(request)
+        user_request = UserRequest(user_sentence=django_req.user_sentence, user_ip=request.client.host, user_request_time=django_req.user_request_time)
+
+        mecab_ner_response = get_mecab_ner_response(user_request.json())
+
+        logger.info({'status': 'success', 'user_ip': request.client.host, "request_path": request.url.path,
+                     "return": mecab_ner_response})
 
         chat_api_response = get_chat_api_response(json.dumps(mecab_ner_response, default=str))
-        c_m_res = ChatMiddlewareResponse(bot_response=chat_api_response)
+
+        logger.info({'status': 'success', 'user_ip': request.client.host, "request_path": request.url.path,
+                     "return": chat_api_response})
+
+        c_m_res = BotResponse(bot_response=chat_api_response)
+
     except ValidationError as e:
-        return e.json()
+        raise Exception
 
     return jsonable_encoder(c_m_res)
 
@@ -38,11 +60,24 @@ async def request_mecab_ner(django_req: Request):
 
 
 @router.post("/test_chat_api")
-async def request_chat_api(chat_api_req: ChatApiRequest):
+async def request_chat_api(chat_api_req: ChatApiRequest, request: Request) -> BotResponse:
+
     try:
+
         chat_api_response = get_chat_api_response(chat_api_req.json())
+
         c_a_res = ChatApiResponse(api_response=chat_api_response, api_server="chat_api_template")
-        return jsonable_encoder(c_a_res)
+
+        logger.info({'status': 'success', 'user_ip': request.client.host, "request_path": request.url.path,
+                     "return": c_a_res})
+
+        c_m_res = BotResponse(bot_response=chat_api_response)
+
+        return jsonable_encoder(c_m_res)
 
     except ValidationError as e:
-        return e.json()
+        raise
+    except DockerNetworkException as dne:
+        raise HTTPException(status_code=418, detail=dne)
+    except Exception as e:
+        logger.critical({'status': 'fail', 'user_ip': request.client.host, "request_path": request.url.path, "message": e})
