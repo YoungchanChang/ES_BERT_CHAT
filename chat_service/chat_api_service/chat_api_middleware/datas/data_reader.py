baeafe0e6cb7106ed2a0636969cdf1c0e249aa30
youngchan <san0558@naver.com>
2022-02-24 17:02:22 +0900

    Update read_template in chat_api_service
    - 템플릿 읽을 때 예외사항 추가

--- chat_service/chat_api_service/chat_api_middleware/datas/data_reader.py
+++ chat_service/chat_api_service/chat_api_middleware/datas/data_reader.py
@@ -1,13 +1,21 @@
 import os
 from pathlib import Path
 
+from utility.custom_error import TemplateNotExist
 
 TEMPLATE_SEP = 2
 
 
 def read_template(template_category):
+
     TEMPLATE_DIR = Path(__file__).resolve().parent.joinpath("chat_templates", f"template_{template_category}.txt")
-    with open(TEMPLATE_DIR, "r", encoding='utf-8-sig') as file:
-        txt_list = file.read().splitlines()
-        template_dictionary = [x.split(",") for x in txt_list if x.count(',') == TEMPLATE_SEP]
-        return sorted(list(template_dictionary), key=len, reverse=True)
+
+    try:
+
+        with open(TEMPLATE_DIR, "r", encoding='utf-8-sig') as file:
+            txt_list = file.read().splitlines()
+            template_dictionary = [x.split(",") for x in txt_list if x.count(',') == TEMPLATE_SEP]
+            return sorted(list(template_dictionary), key=len, reverse=True)
+
+    except FileNotFoundError as fnf:
+        raise TemplateNotExist(fnf)
