8f1cb4ce8266ca37f263b989db3b42cacdf1a9a8
youngchan <san0558@naver.com>
2022-02-24 22:30:10 +0900

    Add RequestValidationError in app.py
    - 예외 처리 함수 추가

--- chat_service/chat_middleware/app.py
+++ chat_service/chat_middleware/app.py
@@ -1,10 +1,12 @@
 import logging.config
 
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, status
+from fastapi.encoders import jsonable_encoder
 from dotenv import load_dotenv
 from fastapi.responses import JSONResponse, PlainTextResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
+from fastapi.exceptions import RequestValidationError
 
 from layer_view import view
 
@@ -24,6 +26,13 @@ app.include_router(view.router)
 async def http_exception_handler(request, exc):
     return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
 
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
+    )
 
 @app.exception_handler(Exception)
 async def exception_general_handler(request: Request, exc: Exception):
