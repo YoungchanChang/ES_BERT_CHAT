749fd0b793142fa669c5badbf4f48ee39fa110ab
youngchan <san0558@naver.com>
2022-02-24 10:22:06 +0900

    Add handlers in app.py
    - critical app 추가

--- chat_service/chat_api_service/chat_api_youtube/app.py
+++ chat_service/chat_api_service/chat_api_youtube/app.py
@@ -13,6 +13,8 @@ from chat_core.settings import chat_log_path
 config_basic['handlers']['access']['filename'] = str(chat_log_path.joinpath('chat_api_youtube', 'access', 'access.log'))
 config_basic['handlers']['warning']['filename'] = str(chat_log_path.joinpath('chat_api_youtube', 'warning', 'warning.log'))
 config_basic['handlers']['error']['filename'] = str(chat_log_path.joinpath('chat_api_youtube', 'error', 'error.log'))
+config_basic['handlers']['critical']['filename'] = str(chat_log_path.joinpath('chat_api_youtube', 'critical', 'critical.log'))
+
 logging.config.dictConfig(config_basic)
 logger = logging.getLogger(__name__)
 
