46e7b329b03942189e82849ae70874a180ddfa60
youngchan <san0558@naver.com>
2022-02-24 09:18:08 +0900

    Update control.py in api_mrc
    - 데코레이터 로그 적용

--- chat_service/chat_api_service/chat_api_mrc/layer_control/control.py
+++ chat_service/chat_api_service/chat_api_mrc/layer_control/control.py
@@ -1,7 +1,15 @@
+import logging
 from typing import List
+from pathlib import Path
 
+from chat_service.chat_core import log_decorator
 from chat_core.chat_domain import MecabNerAttribute
 
-def get_mrc(sentence_attributes: List[MecabNerAttribute]):
+logger = logging.getLogger('decorator')
 
+file_path = str(Path(__file__).resolve().relative_to(Path(__file__).cwd()))
+
+
+@log_decorator.log_basic(logger=logger, path=file_path)
+def get_mrc(sentence_attributes: List[MecabNerAttribute]):
     return "잘 모르겠어요. 제가 공부해서 꼭 알려드릴께요!"
