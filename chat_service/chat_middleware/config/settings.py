a1987999a171e69517acefbe7437842c761e49f2
youngchan <san0558@naver.com>
2022-02-24 22:13:25 +0900

    Add chat_middleware
    - 챗 미들웨어 코어 추가

---
+++ chat_service/chat_middleware/config/settings.py
@@ -0,0 +1,9 @@
+from chat_core.settings import *
+from chat_core.chat_log_config import config_basic
+
+service_name = 'chat_middleware'
+
+config_basic['handlers']['access']['filename'] = str(chat_log_path.joinpath(service_name, 'access', 'access.log'))
+config_basic['handlers']['warning']['filename'] = str(chat_log_path.joinpath(service_name, 'warning', 'warning.log'))
+config_basic['handlers']['error']['filename'] = str(chat_log_path.joinpath(service_name, 'error', 'error.log'))
+config_basic['handlers']['critical']['filename'] = str(chat_log_path.joinpath(service_name, 'critical', 'critical.log'))
