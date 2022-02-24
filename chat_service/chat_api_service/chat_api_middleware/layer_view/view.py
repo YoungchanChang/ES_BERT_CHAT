f2a4178fe936143ce83ec22b0129c8fd0f547238
youngchan <san0558@naver.com>
2022-02-24 16:18:11 +0900

    Update view.py
    - 경로 수정

--- chat_service/chat_api_service/chat_api_middleware/layer_view/view.py
+++ chat_service/chat_api_service/chat_api_middleware/layer_view/view.py
@@ -1,10 +1,18 @@
-from fastapi import APIRouter
+import logging.config
+
+from fastapi import APIRouter, Request
 from fastapi.encoders import jsonable_encoder
 from pydantic import ValidationError
 
-from chat_service.chat_api_service.chat_api_middleware.layer_control.control import get_template_data
-from chat_service.chat_api_service.chat_api_middleware.layer_model.domain import ChatApiRequest, ChatApiResponse, TemplateRequestData
-from chat_service.chat_api_service.chat_api_middleware.utility.api_endpoint import get_youtube_api_response, get_mrc_api_response
+from layer_control.control import get_template_data
+from chat_core.chat_domain import ChatApiRequest, ChatApiResponse, TemplateRequestData
+from utility.api_endpoint import get_youtube_api_response, get_mrc_api_response
+
+from config.settings import config_basic
+
+logging.config.dictConfig(config_basic)
+logger = logging.getLogger('simple_log')
+
 
 BLANK_LIST = 0
 FIRST_VALUE = 0
@@ -15,12 +23,15 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+
 @router.post("/response_api")
-async def request_from_chat_middleware(chat_api_req: ChatApiRequest):
+async def request_from_chat_middleware(chat_api_req: ChatApiRequest, request: Request):
     try:
 
         if len(chat_api_req.sentence_attributes) == BLANK_LIST:
             c_a_res = ChatApiResponse(api_response="잘 모르겠어요. 알려주세요!", api_server="empty_template")
+            logger.info({'status': 'success', 'user_ip': request.client.host, "request_path": request.url.path,
+                         "return": c_a_res})
             return jsonable_encoder(c_a_res)
 
         middle_m_n_a = chat_api_req.sentence_attributes[FIRST_VALUE]
@@ -30,21 +41,29 @@ async def request_from_chat_middleware(chat_api_req: ChatApiRequest):
         if middle_m_n_a.main_category == "music":
             youtube_response = get_youtube_api_response(chat_api_req.json())
             c_a_res = ChatApiResponse(api_response=youtube_response, api_server="youtube_template")
+            logger.info({'status': 'success', 'user_ip': request.client.host, "request_path": request.url.path,
+                         "return": c_a_res})
             return jsonable_encoder(c_a_res)
 
         if middle_m_n_a.main_category == "question":
             mrc_response = get_mrc_api_response(chat_api_req.json())
             c_a_res = ChatApiResponse(api_response="MRC 응답입니다.", api_server="mrc_template")
+            logger.info({'status': 'success', 'user_ip': request.client.host, "request_path": request.url.path,
+                         "return": c_a_res})
             return jsonable_encoder(c_a_res)
 
         api_template = get_template_data(t_r_d)
         if api_template:
             c_a_res = ChatApiResponse(api_response=api_template, api_server="basic_template")
+            logger.info({'status': 'success', 'user_ip': request.client.host, "request_path": request.url.path,
+                         "return": c_a_res})
             return jsonable_encoder(c_a_res)
-    except ValidationError as e:
-        return e.json()
+    except ValidationError as ve:
+        logger.error(
+            {'status': 'fail', 'user_ip': request.client.host, "request_path": request.url.path, "message": ve})
+        return ve.json()
     except Exception as e:
-        print(e)
-        return jsonable_encoder(False)
+        logger.critical({'status': 'fail', 'user_ip': request.client.host, "request_path": request.url.path, "message": e})
+        return jsonable_encoder(e)
     return jsonable_encoder(False)
 
