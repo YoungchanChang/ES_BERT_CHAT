06af4f54215e561263231e4e58e964ff1d68f07b
youngchan <san0558@naver.com>
2022-03-02 14:03:04 +0900

    Add fastapi in mecab_ner_app
    - 엔드포인트 추가

---
+++ chat_service/mecab_service/mecab_ner_app/app/infrastructure/fastapi/mecab_api.py
@@ -0,0 +1,35 @@
+import os
+
+from fastapi import APIRouter
+from fastapi.encoders import jsonable_encoder
+
+
+
+from app.controller.service.bert_sender import get_mecab_bind_feature
+from app.infrastructure.network.api_endpoint import get_bert_confirm_response
+from chat_service.chat_core.chat_domain import UserRequest, ChatApiRequest
+
+router = APIRouter(
+    prefix="/mecab_ner",
+    tags=["mecab_ner"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.post("/mecab_attribute")
+async def get_mecab_attribute(req: UserRequest):
+    m_b_d_list, entity_list, intent_list = get_mecab_bind_feature(sentence=req.user_sentence)
+    bert_request = ChatApiRequest(user_info=req, mecab_bert_bind=m_b_d_list,
+                                  entities=entity_list,
+                                  intents=intent_list)
+    youtube_response = get_bert_confirm_response(bert_request.json())
+
+    return jsonable_encoder(youtube_response)
+
+#
+# @router.post("/mecab_analyzed")
+# async def get_mecab_analyzed(req: Query):
+#     mecab_parsed_list = list(MeCabParser(sentence=req.sentence).gen_mecab_compound_token_feature())
+#     return_json = {"result": mecab_parsed_list}
+#
+#     return jsonable_encoder(return_json)
---
+++ chat_service/mecab_service/mecab_ner_app/app/infrastructure/network/api_endpoint.py
@@ -0,0 +1,29 @@
+import os
+import json
+from pathlib import Path
+
+import requests
+from dotenv import load_dotenv
+
+from app.domain.mecab_exception import BertConfirmException
+from chat_service.chat_core.settings import bert_confirm_url
+
+load_dotenv()
+
+
+def get_bert_confirm_response(json_data):
+
+    try:
+        utility_answer = requests.post(
+            bert_confirm_url, headers={'content-type': 'application/json'}, data=json_data
+        )
+        if utility_answer.status_code == 200:
+            bert_confirm_response = json.loads(utility_answer.text).get("bert_confirm")
+            return bert_confirm_response
+        raise BertConfirmException
+
+    except requests.ConnectionError as ce:
+        # TODO sentry에 오류 메시지 전송
+        print(ce)
+    except Exception as e:
+        print(e)
