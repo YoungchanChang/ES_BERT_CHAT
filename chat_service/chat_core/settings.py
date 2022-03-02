d3a923cf3ab1e25e07ca192ff1bc531b57c07902
youngchan <san0558@naver.com>
2022-03-02 14:02:28 +0900

    Update settings.py in chat_core
    - 엔티티 저장 경로, 버트 요청 경로 추가

--- chat_service/chat_core/settings.py
+++ chat_service/chat_core/settings.py
@@ -7,14 +7,27 @@ chat_api_youtube = "chat_api_youtube"
 chat_api_mrc = "chat_api_mrc"
 mecab_ner = "mecab_ner"
 chat_api_response = "chat_api_middleware"
+entity_storage_path = "/Users/youngchan/Desktop/ES_BERT_CHAT/mecab_ner_app/data/entities/mecab_data"
+intent_path = "/Users/youngchan/Desktop/ES_BERT_CHAT"
+entity_path = "/Users/youngchan/Desktop/ES_BERT_CHAT"
+bert_confirm = "bert_confirm"
+
 if platform.system() == "Darwin":
     chat_api_mrc = "localhost"
     chat_api_youtube = "localhost"
     mecab_ner = "localhost"
     chat_api_response = "localhost"
+    bert_confirm = "localhost"
+    intent_path = "/user/local"
+    entity_path = "/user/local"
+
 
 
 youtube_response_url = f"http://{chat_api_youtube}:5210/chat_api_youtube/response_youtube"
 mrc_response_url = f"http://{chat_api_mrc}:5220/chat_api_mrc/response_mrc"
 mecab_ner = f"http://{mecab_ner}:5100/mecab_ner/mecab_attribute"
 chat_api_response = f"http://{chat_api_response}:5200/chat_api/response_api"
+bert_confirm_url = f"http://{bert_confirm}:5110/mecab_bert/bert_confirm"
+
+intent_mecab_path = f"{intent_path}/mecab_ner_app/data/intents/mecab_data"
+entity_mecab_path = f"{intent_path}/mecab_ner_app/data/entities/mecab_data"
