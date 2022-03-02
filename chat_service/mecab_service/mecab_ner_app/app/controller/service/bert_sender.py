881c6cc1dc3f231e4f9b035a20594e6b4bfcba2f
youngchan <san0558@naver.com>
2022-03-02 13:26:59 +0900

    Add get_mecab_bind_feature in bert_sender.py
    - 메캅 바인드 피쳐 함수로 작성

--- chat_service/mecab_service/mecab_ner_app/app/controller/service/bert_sender.py
+++ chat_service/mecab_service/mecab_ner_app/app/controller/service/bert_sender.py
@@ -9,27 +9,29 @@ from chat_service.chat_core.chat_domain import MecabFeature, MecabSimpleFeature,
 INTENT_POSSIBLE_CATEGORY = []
 
 
-if __name__ == "__main__":
-    intent_category = ["food_like"]
-    test_sentence = "치킨이랑 진주 아이유 듣는 것이 좋다 아이묭 좋아해 동대문 좋아"
+def get_mecab_bind_feature(sentence):
 
     m_b = MecabBinder()
-    bind_result = m_b.get_bind(test_sentence)
+    bind_result = m_b.get_bind(sentence)
 
-    bind_result.bind_list = m_b.split_multi_en_in(test_sentence, bind_result.bind_list)
-    print(bind_result)
-
-    test_intent = [x for x in bind_result.intent_list if x.category.large in intent_category]
-    print(bind_result.bind_list)
-    print(bind_result.intent_list)
-    print(bind_result.entity_list)
-    print(bind_result.noun_list)
+    bind_result.bind_list = m_b.split_multi_en_in(sentence, bind_result.bind_list)
 
     m_b_d_list = [MecabBertBindFeature(bind_category=x.bind_category, bind_sentence=x.bind_sentence,
-                          entity=MecabFeature(value=x.entity.word, large_category=x.entity.category.large, small_category=x.entity.category.small),
-                          intent=MecabFeature(value=x.intent.word, large_category=x.intent.category.large,
-                                              small_category=x.intent.category.small),
-                          ) for x in bind_result.bind_list]
-    entity_list = [MecabFeature(user_sentence=test_sentence, value=x.word, large_category=x.category.large, small_category=x.category.small) for x in bind_result.entity_list]
-    intent_list = [MecabSimpleFeature(user_sentence=test_sentence, value=x.word, large_category=x.category.large, small_category=x.category.small) for x in bind_result.intent_list if x.category.large in intent_category]
-    x = 3
+                                       entity=MecabFeature(value=x.entity.word,
+                                                           large_category=x.entity.category.large,
+                                                           small_category=x.entity.category.small),
+                                       intent=MecabFeature(value=x.intent.word,
+                                                           large_category=x.intent.category.large,
+                                                           small_category=x.intent.category.small),
+                                       ) for x in bind_result.bind_list]
+    entity_list = [MecabSimpleFeature(user_sentence=sentence, value=x.word, large_category=x.category.large,
+                                small_category=x.category.small) for x in bind_result.entity_list]
+    intent_list = [MecabSimpleFeature(user_sentence=sentence, value=x.word, large_category=x.category.large,
+                                      small_category=x.category.small) for x in bind_result.intent_list if
+                   x.category.large in INTENT_POSSIBLE_CATEGORY]
+    return m_b_d_list, entity_list, intent_list
+
+if __name__ == "__main__":
+    sentence = "치킨이랑 진주 아이유 듣는 것이 좋다 아이묭 좋아해 동대문 좋아"
+    value = get_mecab_bind_feature(sentence)
+    a = 3
