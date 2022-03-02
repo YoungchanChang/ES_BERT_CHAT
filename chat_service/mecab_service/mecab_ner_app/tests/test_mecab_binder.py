6d898a9cd26c5cc06784dab7245b46e6c3465332
youngchan <san0558@naver.com>
2022-03-02 13:36:37 +0900

    Update test_bert_send in test_mecab_binder.py
    - bert_send 테스트 코드 추가

--- chat_service/mecab_service/mecab_ner_app/tests/test_mecab_binder.py
+++ chat_service/mecab_service/mecab_ner_app/tests/test_mecab_binder.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 
+from app.controller.service.bert_sender import get_mecab_bind_feature
 from app.controller.service.mecab_binder import MecabEntity, MecabIntent, MecabBinder
 from app.domain.mecab_domain import BindResult, BindInfo
+from chat_service.chat_core.chat_domain import MecabBertBindFeature, MecabFeature, MecabSimpleFeature
 from domain.mecab_domain import Category, MecabNerFeature
 
 
@@ -47,3 +49,22 @@ def test_get_bind():
     multi_en_in = m_b.split_multi_en_in(test_sentence, bind_result.bind_list)
 
     assert multi_en_in == [BindInfo(bind_category='music', entity=MecabNerFeature(word='진주 아이유', pos='entity', start_idx=3, end_idx=5, category=Category(large='music_singer', small='가수')), intent=MecabNerFeature(word='듣는 것이 좋다', pos='intent', start_idx=5, end_idx=11, category=Category(large='music_like', small='듣는게 좋다')), end_idx=11, split_sentence='동대문에서 진주 아이유 듣는 것이 좋다'), BindInfo(bind_category='music', entity=MecabNerFeature(word='아이묭', pos='entity', start_idx=11, end_idx=13, category=Category(large='music_singer', small='가수')), intent=MecabNerFeature(word='듣는 것이 좋다', pos='intent', start_idx=13, end_idx=19, category=Category(large='music_like', small='듣는게 좋다')), end_idx=19, split_sentence='아이묭 듣는 것이 좋다')]
+
+
+def test_bert_send():
+    entity_path = Path(__file__).resolve().parent.parent.joinpath("data", "entities", "entity_data")
+    intent_path = Path(__file__).resolve().parent.parent.joinpath("data", "intents", "intent_data")
+
+    m_b = MecabBinder(entity_path=str(entity_path), intent_path=str(intent_path))
+    sentence = "치킨이랑 진주 아이유 듣는 것이 좋다 아이묭 좋아해 동대문 좋아"
+    value = get_mecab_bind_feature(sentence=sentence, mecab_binder=m_b)
+    m_b_d_list, entity_list, intent_list = value
+    assert m_b_d_list == [MecabBertBindFeature(bind_category='music', bind_sentence='진주 아이유 듣는 것이 좋다', entity=MecabFeature(value='진주 아이유', large_category='music_singer', small_category='가수'), intent=MecabFeature(value='듣는 것이 좋다', large_category='music_like', small_category='듣는게 좋다'), bert_confirm=False), MecabBertBindFeature(bind_category='music', bind_sentence='아이묭 좋아해', entity=MecabFeature(value='아이묭', large_category='music_singer', small_category='가수'), intent=MecabFeature(value='좋아해', large_category='music_like', small_category='좋다'), bert_confirm=False)]
+    assert entity_list == [MecabSimpleFeature(value='치킨', large_category='fastfood', small_category='fastfood', user_sentence='치킨이랑 진주 아이유 듣는 것이 좋다 아이묭 좋아해 동대문 좋아', bert_confirm=False)]
+    assert intent_list == []
+
+    value = get_mecab_bind_feature(sentence=sentence, mecab_binder=m_b, intent_category=["food_like"])
+    m_b_d_list, entity_list, intent_list = value
+    assert m_b_d_list == [MecabBertBindFeature(bind_category='music', bind_sentence='진주 아이유 듣는 것이 좋다', entity=MecabFeature(value='진주 아이유', large_category='music_singer', small_category='가수'), intent=MecabFeature(value='듣는 것이 좋다', large_category='music_like', small_category='듣는게 좋다'), bert_confirm=False), MecabBertBindFeature(bind_category='music', bind_sentence='아이묭 좋아해', entity=MecabFeature(value='아이묭', large_category='music_singer', small_category='가수'), intent=MecabFeature(value='좋아해', large_category='music_like', small_category='좋다'), bert_confirm=False)]
+    assert entity_list == [MecabSimpleFeature(value='치킨', large_category='fastfood', small_category='fastfood', user_sentence='치킨이랑 진주 아이유 듣는 것이 좋다 아이묭 좋아해 동대문 좋아', bert_confirm=False)]
+    assert intent_list == [MecabSimpleFeature(value='좋아해', large_category='food_like', small_category='좋다', user_sentence='치킨이랑 진주 아이유 듣는 것이 좋다 아이묭 좋아해 동대문 좋아', bert_confirm=False), MecabSimpleFeature(value='좋아해', large_category='food_like', small_category='좋아한다', user_sentence='치킨이랑 진주 아이유 듣는 것이 좋다 아이묭 좋아해 동대문 좋아', bert_confirm=False), MecabSimpleFeature(value='좋', large_category='food_like', small_category='좋다', user_sentence='치킨이랑 진주 아이유 듣는 것이 좋다 아이묭 좋아해 동대문 좋아', bert_confirm=False)]
