66aa294c19c06b2595008d8c1f8347d97b2a95df
youngchan <san0558@naver.com>
2022-03-02 17:44:09 +0900

    Update data_api.py
    - 데이터 api 추가

--- chat_service/mecab_service/mecab_ner_app/app/infrastructure/fastapi/data_api.py
+++ chat_service/mecab_service/mecab_ner_app/app/infrastructure/fastapi/data_api.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 from fastapi.encoders import jsonable_encoder
 
-from app.controller.service.mecab_controller import MecabDataController
-from app.domain.entity import CategoryItem, MecabWord
+from app.application.service.django_data import DjangoDataController
+from app.domain.data_domain import CategoryIndex, MecabWord
 
 router = APIRouter(
     prefix="/mecab_data",
@@ -12,14 +12,14 @@ router = APIRouter(
 
 
 @router.post("/create_index")
-async def create_index(category_item: CategoryItem):
-    is_index_created = MecabDataController.create_index(category_item)
+async def create_index(category_index: CategoryIndex):
+    is_index_created = DjangoDataController.create_index(category_index)
     return_json = {"result": is_index_created}
     return jsonable_encoder(return_json)
 
 
 @router.post("/insert_data")
 async def insert_data(mecab_word: MecabWord):
-    is_index_created = MecabDataController.insert_data(mecab_word)
+    is_index_created = DjangoDataController.insert_data(mecab_word)
     return_json = {"result": is_index_created}
     return jsonable_encoder(return_json)
--- chat_service/mecab_service/mecab_ner_app/app/infrastructure/fastapi/main.py
+++ chat_service/mecab_service/mecab_ner_app/app/infrastructure/fastapi/main.py
@@ -5,10 +5,10 @@ from app.infrastructure.fastapi import mecab_api, data_api
 app = FastAPI()
 
 app.include_router(mecab_api.router)
-# app.include_router(data_api.router)
+app.include_router(data_api.router)
 
 
 if __name__ == "__main__":
     uvicorn.run(
-        "app.infrastructure.fastapi.main:app", host="0.0.0.0", port=5100, reload=True
+        "app.infrastructure.fastapi.main:app", host="0.0.0.0", port=5101, reload=True
     )
