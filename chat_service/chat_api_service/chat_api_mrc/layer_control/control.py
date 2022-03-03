d00f61f05053dfaa6c2a9506edd7ccfe405abe9d
youngchan <san0558@naver.com>
2022-03-04 05:56:54 +0900

    Update chat_api_mrc
    - rename 진행

--- chat_service/chat_api_service/chat_api_mrc/layer_control/control.py
+++ chat_service/chat_api_service/chat_api_mrc/layer_control/control.py
@@ -12,6 +12,6 @@ logger = logging.getLogger('decorator')
 file_path = str(Path(__file__).resolve().relative_to(Path(__file__).cwd()))
 
 
-@chat_decorator.log_basic(logger=logger, path=file_path)
+@log_decorater.log_basic(logger=logger, path=file_path)
 def get_mrc(sentence_attributes: str):
     return "잘 모르겠어요. 제가 공부해서 꼭 알려드릴께요!"
