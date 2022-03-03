3105b48aa2cc8de2ecd7255d82d1aa3135c9130e
youngchan <san0558@naver.com>
2022-03-03 09:09:57 +0900

    Update manage.py
    - 개발 / 운영 url 포트 변경

--- django_web/manage.py
+++ django_web/manage.py
@@ -26,17 +26,17 @@ if __name__ == '__main__':
     if platform.system() == "Darwin":
         os.environ["RDS_PORT"] = "3307"
         os.environ["RDS_HOST"] = "127.0.0.1"
-        bot_response_url = "localhost"
+        bot_response_url = "localhost:5001"
         mecab_create_index = "localhost:5101"
         mecab_insert_data = "localhost:5101"
     else:
         os.environ["RDS_PORT"] = "3306"
         os.environ["RDS_HOST"] = "mysql_db"
-        bot_response_url = "chat_middleware"
+        bot_response_url = "chat_middleware:5000"
         mecab_create_index = "mecab_ner_app:5100"
         mecab_insert_data = "mecab_insert_data:5100"
 
-    os.environ["bot_response"] = f"http://{bot_response_url}:5000/chat_middleware/response_middleware"
+    os.environ["bot_response"] = f"http://{bot_response_url}/chat_middleware/response_middleware"
     os.environ["mecab_create_index"] = f"http://{mecab_create_index}/mecab_data/create_index"
     os.environ["mecab_insert_data"] = f"http://{mecab_insert_data}/mecab_data/insert_data"
 
