d8909265a7ba27c2a98489407e05bc8605e925d9
youngchan <san0558@naver.com>
2022-03-04 05:17:48 +0900

    Update template_insert_url in manage.py
    - 아이템, 카테고리 삽입 URL 추가

--- django_web/manage.py
+++ django_web/manage.py
@@ -29,16 +29,22 @@ if __name__ == '__main__':
         bot_response_url = "localhost:5001"
         mecab_create_index = "localhost:5101"
         mecab_insert_data = "localhost:5101"
+        insert_template_category = "localhost:5201"
+        insert_template_item = "localhost:5201"
     else:
         os.environ["RDS_PORT"] = "3306"
         os.environ["RDS_HOST"] = "mysql_db"
         bot_response_url = "chat_middleware:5000"
         mecab_create_index = "mecab_ner_app:5100"
         mecab_insert_data = "mecab_insert_data:5100"
+        insert_template_category = "localhost:5200"
+        insert_template_item = "localhost:5200"
 
     os.environ["bot_response"] = f"http://{bot_response_url}/chat_middleware/response_middleware"
     os.environ["mecab_create_index"] = f"http://{mecab_create_index}/mecab_data/create_index"
     os.environ["mecab_insert_data"] = f"http://{mecab_insert_data}/mecab_data/insert_data"
+    os.environ["insert_template_category"] = f"http://{insert_template_category}/chat_api/insert_template_category"
+    os.environ["insert_template_item"] = f"http://{insert_template_item}/chat_api/insert_template_item"
 
 
     main()
