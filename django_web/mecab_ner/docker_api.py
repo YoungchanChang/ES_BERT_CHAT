cc7a22753966673392b9af12d21d6f1a688323e7
youngchan <san0558@naver.com>
2022-03-04 05:17:11 +0900

    Add insert_template_item in docker_api.py
    - 아이템, 카테고리 삽입 함수 추가

--- django_web/mecab_ner/docker_api.py
+++ django_web/mecab_ner/docker_api.py
@@ -23,3 +23,23 @@ def insert_mecab_data(json_data):
     answer = json.loads(utility_answer.text).get("answer")
 
     return answer
+
+def insert_template_item(json_data):
+
+    utility_answer = requests.post(
+        os.getenv("insert_template_item"), headers={'content-type': 'application/json'}, data=json.dumps(json_data), timeout=3
+    )
+
+    answer = json.loads(utility_answer.text).get("result")
+
+    return answer
+
+def insert_template_category(json_data):
+
+    utility_answer = requests.post(
+        os.getenv("insert_template_category"), headers={'content-type': 'application/json'}, data=json.dumps(json_data), timeout=3
+    )
+
+    answer = json.loads(utility_answer.text).get("result")
+
+    return answer
