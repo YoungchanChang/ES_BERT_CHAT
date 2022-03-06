26e03da44f33f23f3806b3b9ab0195a0cc535816
youngchan <san0558@naver.com>
2022-03-06 14:39:27 +0900

    Add entity_item path
    - 아이템 경로 추가

--- django_web/mecab_ner/urls.py
+++ django_web/mecab_ner/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [path("", views.index, name="list"),
                path("entity_category", views.EntityCategoryItemView.as_view(), name="entity_category"),
                path("entity_category_search/", views.EntityCategorySearchView.as_view(), name="entity_category_search"),
                path("entity_category_add/", views.EntityCategoryAddView.as_view(), name="entity_category_add"),
+               path("entity_item/", views.EntityItemView.as_view(), name="entity_item"),
                ]
