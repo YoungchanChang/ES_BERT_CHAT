c44a6288690459653ce593c082a3029328111463
youngchan <san0558@naver.com>
2022-03-06 17:33:18 +0900

    Update urls.py in mecab_ner
    - entity_item_add 추가

--- django_web/mecab_ner/urls.py
+++ django_web/mecab_ner/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [path("", views.index, name="list"),
                path("entity_category_search/", views.EntityCategorySearchView.as_view(), name="entity_category_search"),
                path("entity_category_add/", views.EntityCategoryAddView.as_view(), name="entity_category_add"),
                path("entity_item/", views.EntityItemView.as_view(), name="entity_item"),
+               path("entity_item_search/", views.EntityItemSearchView.as_view(), name="entity_item_search"),
+               path("entity_item_add/", views.EntityItemAddView.as_view(), name="entity_item_add"),
                ]
