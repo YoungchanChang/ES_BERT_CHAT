4eeaf1a1bb8c35246c2cb15176249b87769eaede
youngchan <san0558@naver.com>
2022-03-09 16:00:01 +0900

    Update manage.py
    - settings.py 반영할 수 있도록 변경

--- django_web/config/urls.py
+++ django_web/config/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('admin_mysite_no_one_allow/', admin.site.urls),
 ]
 
-if settings.DEBUG is False:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
