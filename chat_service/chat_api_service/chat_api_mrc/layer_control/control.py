ba57651836bebd61ef309e0a7bfe016f5a04d0e5
youngchan <san0558@naver.com>
2022-03-09 13:14:53 +0900

    Updtae control.py
    - 데코레이터 임시 비활성화

--- chat_service/chat_api_service/chat_api_mrc/layer_control/control.py
+++ chat_service/chat_api_service/chat_api_mrc/layer_control/control.py
@@ -1,7 +1,7 @@
 import logging.config
 from pathlib import Path
 
-from chat_core import log_decorater
+# from chat_core import log_decorater
 
 from config.settings import config_basic
 
@@ -12,6 +12,6 @@ logger = logging.getLogger('decorator')
 file_path = str(Path(__file__).resolve().relative_to(Path(__file__).cwd()))
 
 
-@log_decorater.log_basic(logger=logger, path=file_path)
+# @log_decorater.log_basic(logger=logger, path=file_path)
 def get_mrc(sentence_attributes: str):
     return "잘 모르겠어요. 제가 공부해서 꼭 알려드릴께요!"
