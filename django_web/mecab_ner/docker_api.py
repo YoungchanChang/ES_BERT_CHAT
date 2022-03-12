35849db19ba44a4428a78a9a689ca71c2e905a79
youngchan <san0558@naver.com>
2022-03-12 20:01:31 +0900

    Update django_web
    - 장고 웹 로그 기능 업데이트

--- django_web/mecab_ner/docker_api.py
+++ django_web/mecab_ner/docker_api.py
@@ -1,13 +1,10 @@
 import os
 import json
 import requests
-import logging
-formatter = "%(asctime)s.%(msecs)03d\t%(levelname)s\t[%(name)s]\t%(message)s"
-logging.basicConfig(level=logging.WARNING, format=formatter, datefmt='%m/%d/%Y %I:%M:%S')
+
 
 def create_mecab_index(json_data):
 
-    logging.WARNING({"mecab_create_index" : os.getenv("mecab_create_index")})
 
     utility_answer = requests.post(
         os.getenv("mecab_create_index"), headers={'content-type': 'application/json'}, data=json.dumps(json_data), timeout=3
@@ -21,7 +18,6 @@ def create_mecab_index(json_data):
 
 def insert_mecab_data(json_data):
 
-    logging.WARNING({"mecab_insert_data": os.getenv("mecab_insert_data")})
 
     utility_answer = requests.post(
         os.getenv("mecab_insert_data"), headers={'content-type': 'application/json'}, data=json.dumps(json_data), timeout=3
@@ -34,7 +30,6 @@ def insert_mecab_data(json_data):
 
 def insert_template_item(json_data):
 
-    logging.WARNING({"insert_template_item": os.getenv("insert_template_item")})
 
     utility_answer = requests.post(
         os.getenv("insert_template_item"), headers={'content-type': 'application/json'}, data=json.dumps(json_data), timeout=3
@@ -46,7 +41,6 @@ def insert_template_item(json_data):
 
 def insert_template_category(json_data):
 
-    logging.WARNING({"insert_template_category": os.getenv("insert_template_category")})
 
     utility_answer = requests.post(
         os.getenv("insert_template_category"), headers={'content-type': 'application/json'}, data=json.dumps(json_data), timeout=3
