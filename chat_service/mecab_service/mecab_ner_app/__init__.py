05d8183dad2e81609160e0731463145a19238582
youngchan <san0558@naver.com>
2022-03-13 20:50:17 +0900

    Update __init__.py
    - 경로 수정

--- chat_service/mecab_service/mecab_ner_app/__init__.py
+++ chat_service/mecab_service/mecab_ner_app/__init__.py
@@ -1,1 +1,1 @@
-from chat_service.mecab_service.mecab_ner_app.app.mecab_parser import MecabParser as MecabParser
+from chat_service.mecab_service.mecab_ner_app.app.application.service.mecab_parser import MecabParser
